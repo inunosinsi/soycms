05c32d4c272e916b4741acbc14d2cdb7554cc689
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2016-05-17 10:37:36 +0900

    ソートボタン設置プラグインでカスタムフィールドの値でソート分を追加

--- cms/soyshop/webapp/src/module/plugins/common_sort_button/util/SortButtonUtil.class.php
+++ cms/soyshop/webapp/src/module/plugins/common_sort_button/util/SortButtonUtil.class.php
@@ -8,7 +8,7 @@ class SortButtonUtil{
 	
 	//ソートに使うキーリスト
 	public static function getColumnList(){		
-		return array("id" => "商品ID",
+		$list = array("id" => "商品ID",
 					"name" => "商品名",
 					"code" => "商品コード",
 					"stock" => "在庫数",
@@ -16,6 +16,17 @@ class SortButtonUtil{
 					"cdate" => "商品の登録日",
 					"udate" => "商品情報の更新日"
 				);
+		
+		//カスタムフィールド		
+		SOY2::import("domain.shop.SOYShop_ItemAttribute");
+		foreach(SOYShop_ItemAttributeConfig::load(true) as $field){
+			$conf = $field->getConfig();
+			if(isset($conf) && is_array($conf) && isset($conf["isIndex"]) && $conf["isIndex"] == 1){
+				$list["custom_" . $field->getFieldId()] = $field->getLabel() . "(カスタムフィールド)";
+			}
+		}
+				
+		return $list;
 	}
 }
 
