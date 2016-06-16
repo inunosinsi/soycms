2901257bee832c3a49990f986b4b4bc3bdfd138a
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2016-06-16 11:47:55 +0900

    キャンペーンプラグインを追加

---
+++ cms/soyshop/webapp/src/module/plugins/campaign/logic/CampaignLogic.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class CampaignLogic extends SOY2LogicBase{
+	
+	function CampaignLogic(){
+		SOY2::imports("module.plugins.campaign.domain.*");
+	}
+	
+	function getCampaignByIdWithinPostPeriod($campaignId){
+		try{
+			$campaign = self::dao()->getById($campaignId);
+		}catch(Exception $e){
+			return new SOYShop_Campaign();
+		}
+		
+		//削除フラグを調べる
+		if($campaign->getIsDisabled() != SOYShop_Campaign::NO_DISABLED)  return new SOYShop_Campaign();
+		
+		//公開状態を調べる
+		if($campaign->getIsOpen() != SOYShop_Campaign::IS_OPEN) return new SOYShop_Campaign();
+		
+		//公開期限を調べる
+		if($campaign->getPostPeriodStart() > time() || $campaign->getPostPeriodEnd() < time()) return new SOYShop_Campaign();
+		
+		return $campaign;
+	}
+	
+	private function dao(){
+		static $dao;
+		if(is_null($dao)) $dao = SOY2DAOFactory::create("SOYShop_CampaignDAO");
+		return $dao;
+	}
+}
+?>
