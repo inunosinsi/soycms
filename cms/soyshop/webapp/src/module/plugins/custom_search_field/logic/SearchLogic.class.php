60f05b5d56bab418375625d582703dc212291c52
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2016-10-19 16:24:15 +0900

    カスタムサーチフィールドでフィールドの値で小商品が検索出来なかった不具合を修正

--- cms/soyshop/webapp/src/module/plugins/custom_search_field/logic/SearchLogic.class.php
+++ cms/soyshop/webapp/src/module/plugins/custom_search_field/logic/SearchLogic.class.php
@@ -18,10 +18,8 @@ class SearchLogic extends SOY2LogicBase{
 	function search($obj, $current, $limit){
 		self::setCondition();
 		
-		$sql = "SELECT i.* " .
-				"FROM soyshop_item i " .
-				"INNER JOIN soyshop_custom_search s ".
-				"ON i.id = s.item_id ";
+		$sql = "SELECT * " .
+				"FROM soyshop_item ";
 		$sql .= self::buildWhere();	//カウントの時と共通の処理は切り分ける
 		
 		$sort = SOY2Logic::createInstance("logic.shop.item.SearchItemUtil", array("sort" => $obj))->getSortQuery();
@@ -53,10 +51,8 @@ class SearchLogic extends SOY2LogicBase{
 	function getTotal(){
 		self::setCondition();
 		
-		$sql = "SELECT COUNT(i.id) AS total " .
-				"FROM soyshop_item i " .
-				"INNER JOIN soyshop_custom_search s ".
-				"ON i.id = s.item_id ";
+		$sql = "SELECT COUNT(id) AS total " .
+				"FROM soyshop_item ";
 		$sql .= self::buildWhere();	//カウントの時と共通の処理は切り分ける
 		
 		try{
@@ -71,31 +67,31 @@ class SearchLogic extends SOY2LogicBase{
 	private function buildWhere(){
 		$config = CustomSearchFieldUtil::getSearchConfig();
 		
-		$where = "WHERE i.open_period_start < :now ".
-				"AND i.open_period_end > :now ".
-				"AND i.item_is_open = 1 ".
-				"AND i.is_disabled != 1 ";
+		$where = "WHERE open_period_start < :now ".
+				"AND open_period_end > :now ".
+				"AND item_is_open = 1 ".
+				"AND is_disabled != 1 ";
 		
 		$item_where = array();
 		
 		//通常商品を表示
 		if(isset($config["search"]["single"]) && (int)$config["search"]["single"] === 1){
-			$item_where[] = "i.item_type = \"" . SOYShop_Item::TYPE_SINGLE . "\"";
+			$item_where[] = "item_type = \"" . SOYShop_Item::TYPE_SINGLE . "\"";
 		}
 		
 		//親商品を表示
 		if(isset($config["search"]["parent"]) && (int)$config["search"]["parent"] === 1){
-			$item_where[] = "i.item_type = \"" . SOYShop_Item::TYPE_GROUP . "\"";
+			$item_where[] = "item_type = \"" . SOYShop_Item::TYPE_GROUP . "\"";
 		}
 		
 		//小商品を表示
 		if(isset($config["search"]["child"]) && (int)$config["search"]["child"] === 1){
-			$item_where[] = "i.item_type REGEXP '^[0-9]+$'";
+			$item_where[] = "item_type REGEXP '^[0-9]+$'";
 		}
 		
 		//ダウンロード商品を表示
 		if(isset($config["search"]["download"]) && (int)$config["search"]["download"] === 1){
-			$item_where[] = "i.item_type = \"" . SOYShop_Item::TYPE_DOWNLOAD . "\"";
+			$item_where[] = "item_type = \"" . SOYShop_Item::TYPE_DOWNLOAD . "\"";
 		}
 		
 		if(count($item_where)){
@@ -114,7 +110,7 @@ class SearchLogic extends SOY2LogicBase{
 			//SOYShop_Itemの値
 			foreach(array("item_name", "item_code") as $key){
 				if(isset($_GET["c_search"][$key]) && strlen($_GET["c_search"][$key])) {
-					$this->where[$key] = "i." . $key . " LIKE :" . $key;
+					$this->where[$key] = $key . " LIKE :" . $key;
 					$this->binds[":" . $key] = "%" . trim($_GET["c_search"][$key]) . "%";
 				}
 			}
@@ -125,18 +121,18 @@ class SearchLogic extends SOY2LogicBase{
 				$maps = SOY2DAOFactory::create("shop.SOYShop_CategoryDAO")->getMapping();
 				$catId = (int)trim($_GET["c_search"]["item_category"]);
 				if(isset($maps[$catId])){
-					$this->where["item_category"] = "i.item_category IN (" . implode(",", $maps[$catId]) . ")";
+					$this->where["item_category"] = "item_category IN (" . implode(",", $maps[$catId]) . ")";
 				}
 			}
 			
 			$pmin = "";$pmax = "";
 			if(isset($_GET["c_search"]["item_price_min"]) && strlen($_GET["c_search"]["item_price_min"]) && is_numeric($_GET["c_search"]["item_price_min"])) {
-				$pmin = "i.item_price >= :item_price_min";
+				$pmin = "item_price >= :item_price_min";
 				$this->binds[":item_price_min"] = (int)$_GET["c_search"]["item_price_min"];
 			}
 			
 			if(isset($_GET["c_search"]["item_price_max"]) && strlen($_GET["c_search"]["item_price_max"]) && is_numeric($_GET["c_search"]["item_price_max"])) {
-				$pmax = "i.item_price <= :item_price_max";
+				$pmax = "item_price <= :item_price_max";
 				$this->binds[":item_price_max"] = (int)$_GET["c_search"]["item_price_max"];
 			}
 			
@@ -146,6 +142,8 @@ class SearchLogic extends SOY2LogicBase{
 				$this->where["item_price"] = $pmin . $pmax;
 			}
 			
+			$s_where = array();
+			
 			foreach(CustomSearchFieldUtil::getConfig() as $key => $field){
 		
 				//まずは各タイプのfield SQLでkeyを指定する場合、s.を付けること。soyshop_custom_searchのaliasがs
@@ -153,7 +151,7 @@ class SearchLogic extends SOY2LogicBase{
 					//文字列の場合
 					case CustomSearchFieldUtil::TYPE_STRING:
 						if(isset($_GET["c_search"][$key]) && strlen($_GET["c_search"][$key])){
-							$this->where[$key] = "s." . $key . " LIKE :" . $key;
+							$s_where[$key] = $key . " LIKE :" . $key;
 							$this->binds[":" . $key] = "%" . trim($_GET["c_search"][$key]) . "%";
 						}
 						break;
@@ -162,17 +160,17 @@ class SearchLogic extends SOY2LogicBase{
 					case CustomSearchFieldUtil::TYPE_RANGE:
 						$ws = "";$we = "";	//whereのスタートとエンド
 						if(isset($_GET["c_search"][$key . "_start"]) && strlen($_GET["c_search"][$key . "_start"]) && is_numeric($_GET["c_search"][$key . "_start"])){
-							$ws = "s." . $key . " >= :" . $key . "_start";
+							$ws = $key . " >= :" . $key . "_start";
 							$this->binds[":" . $key . "_start"] = (int)$_GET["c_search"][$key . "_start"];
 						}
 						if(isset($_GET["c_search"][$key . "_end"]) && strlen($_GET["c_search"][$key . "_end"]) && is_numeric($_GET["c_search"][$key . "_end"])){
-							$we = "s." . $key .  " <= :" . $key . "_end";
-							$this->binds[":" . $key . "_end"] = (int)$_GET["c_search"][$key . "_end"];
+							$we = $key .  " <= :" . $key . "_end";
+							$s_binds[":" . $key . "_end"] = (int)$_GET["c_search"][$key . "_end"];
 						}
 						if(strlen($ws) && strlen($we)){
-							$this->where[$key] = "(" . $ws . " AND " . $we . ")";
+							$s_where[$key] = "(" . $ws . " AND " . $we . ")";
 						}else{
-							$this->where[$key] = $ws . $we;
+							$s_where[$key] = $ws . $we;
 						}
 						break;
 						
@@ -182,23 +180,57 @@ class SearchLogic extends SOY2LogicBase{
 							$w = array();
 							foreach($_GET["c_search"][$key] as $i => $v){
 								if(!strlen($v)) continue;
-								$w[] = "s." . $key . " LIKE :" . $key . $i;
+								$w[] = $key . " LIKE :" . $key . $i;
 								$this->binds[":" . $key . $i] = "%" . trim($v) . "%";
 							}
-							if(count($w)) $this->where[$key] = "(" . implode(" OR ", $w) . ")";
+							if(count($w)) $s_where[$key] = "(" . implode(" OR ", $w) . ")";
 						}
 						break;
 					
 					//数字、ラジオボタン、セレクトボックス
 					default:
 						if(isset($_GET["c_search"][$key]) && strlen($_GET["c_search"][$key])){
-							$this->where[$key] = "s." . $key . " = :" . $key;
+							$s_where[$key] = $key . " = :" . $key;
 							$this->binds[":" . $key] = $_GET["c_search"][$key];
 						}
 				}
 			}
 			$this->binds[":now"] = time();
+			
+			if(count($s_where)){
+				$subquery = "(" .
+						"SELECT item_id FROM soyshop_custom_search WHERE ";
+				$f = 0;
+				foreach($s_where as $sw){
+					if(!strlen($sw)) continue;
+					if($f == 0){
+						$subquery .= $sw . " ";
+					}else{
+						$subquery .= "AND " . $sw . " ";
+					}
+					
+					$f++;
+				}
+				$subquery .= ")";
+				$config = CustomSearchFieldUtil::getSearchConfig();
+				$w = "(id IN " . $subquery;
+				if(isset($config["search"]["child"]) && (int)$config["search"]["child"] === 1){
+					$w .= " OR item_type IN " . $subquery;
+				}
+				$w .= ")";
+				
+				$this->where["custom"] = $w;
+			}
 		}	
+	}
+	
+	private function buildListWhere(){
+		
+		return "WHERE i.open_period_start < :now ".
+				"AND i.open_period_end > :now ".
+				"AND i.item_is_open = 1 ".
+				"AND i.is_disabled != 1 ".
+				"AND i.item_type IN (\"" . SOYShop_Item::TYPE_SINGLE . "\",\"" . SOYShop_Item::TYPE_GROUP . "\",\"" . SOYShop_Item::TYPE_DOWNLOAD . "\") ";
 	}
 	
 	/** 商品一覧ページ用 **/
@@ -210,10 +242,10 @@ class SearchLogic extends SOY2LogicBase{
 		$binds = array(":now" => time());
 		
 		$sql = "SELECT i.* " .
-				"FROM soyshop_item i " .
+				"FROM soyshop_item i ".
 				"INNER JOIN soyshop_custom_search s ".
 				"ON i.id = s.item_id ";
-		$sql .= self::buildWhere();	//カウントの時と共通の処理は切り分ける
+		$sql .= self::buildListWhere();	//カウントの時と共通の処理は切り分ける
 		switch($confs[$key]["type"]){
 			case CustomSearchFieldUtil::TYPE_CHECKBOX:
 				$sql .= "AND s." . $key . " LIKE :" . $key;
@@ -225,7 +257,7 @@ class SearchLogic extends SOY2LogicBase{
 		}
 		
 		$sort = SOY2Logic::createInstance("logic.shop.item.SearchItemUtil", array("sort" => $obj))->getSortQuery();
-		$sql .= " ORDER BY " . $sort . " ";
+		$sql .= " ORDER BY i." . $sort . " ";
 		
 		$sql .= " LIMIT " . $limit;
 		
@@ -256,11 +288,11 @@ class SearchLogic extends SOY2LogicBase{
 		
 		$binds = array(":now" => time());
 		
-		$sql = "SELECT COUNT(i.id) AS total " .
-				"FROM soyshop_item i " .
+		$sql = "SELECT COUNT(i.id) " .
+				"FROM soyshop_item i ".
 				"INNER JOIN soyshop_custom_search s ".
 				"ON i.id = s.item_id ";
-		$sql .= self::buildWhere();	//カウントの時と共通の処理は切り分ける
+		$sql .= self::buildListWhere();	//カウントの時と共通の処理は切り分ける
 		switch($confs[$key]["type"]){
 			case CustomSearchFieldUtil::TYPE_CHECKBOX:
 				$sql .= "AND s." . $key . " LIKE :" . $key;
