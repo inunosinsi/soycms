d062320e59abc1978b6ab2b3015ebf5edb70b95c
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2016-11-18 13:53:11 +0900

    エクスポートプラグインでタイトルを空にした時、ボタンを表示させないように修正

--- cms/soyshop/webapp/pages/Order/IndexPage.class.php
+++ cms/soyshop/webapp/pages/Order/IndexPage.class.php
@@ -118,7 +118,7 @@ class IndexPage extends WebPage{
 
 		/* 出力用 */
 		$moduleList = $this->getExportModuleList();
-		$this->createAdd("module_list", "ExportModuleList", array(
+		$this->createAdd("module_list", "_common.Order.ExportModuleListComponent", array(
 			"list" => $moduleList
 		));
 
@@ -643,35 +643,6 @@ class SearchForm extends SOYBodyComponentBase{
 	}
 	function setPaymentMethod($paymentMethod){
 		$this->paymentMethod = $paymentMethod;
-	}
-}
-
-class ExportModuleList extends HTMLList{
-
-	private $exportPageLink;
-
-	protected function populateItem($entity,$key){
-		$this->addInput("module_id", array(
-			/*"label" => "選択する",*/
-			"name" => "plugin",
-			"value" => $key,
-		));
-
-		$this->addLabel("export_title", array(
-			"text" => $entity["title"],
-		));
-
-		$this->addLabel("export_description", array(
-			"html" => $entity["description"],
-			"visible" => (strlen($entity["description"]) > 0)
-		));
-	}
-
-	function getExportPageLink() {
-		return $this->exportPageLink;
-	}
-	function setExportPageLink($exportPageLink) {
-		$this->exportPageLink = $exportPageLink;
 	}
 }
 ?>
---
+++ cms/soyshop/webapp/pages/_common/Order/ExportModuleListComponent.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class ExportModuleListComponent extends HTMLList{
+	
+	private $exportPageLink;
+
+	protected function populateItem($entity,$key){
+		$this->addInput("module_id", array(
+			/*"label" => "選択する",*/
+			"name" => "plugin",
+			"value" => $key,
+		));
+
+		$this->addLabel("export_title", array(
+			"text" => $entity["title"],
+		));
+
+		$this->addLabel("export_description", array(
+			"html" => $entity["description"],
+			"visible" => (strlen($entity["description"]) > 0)
+		));
+		
+		$this->addLabel("display_button", array(
+			"visible" => (strlen($entity["title"]))
+		));
+	}
+
+	function getExportPageLink() {
+		return $this->exportPageLink;
+	}
+	function setExportPageLink($exportPageLink) {
+		$this->exportPageLink = $exportPageLink;
+	}
+}
