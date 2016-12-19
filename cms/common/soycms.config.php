7b41422effd9ec3f764b07cca92192e98d951068
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2016-12-19 15:58:14 +0900

    SOYShop連携プラグインのPHP7対応

--- cms/common/soycms.config.php
+++ cms/common/soycms.config.php
@@ -1,8 +1,8 @@
 <?php
 define("SOYCMS_DB_TYPE","mysql");
 //define("SOY2HTML_CACHE_FORCE",false);
-define("SOYCMS_VERSION","1.8.12p7.36");
-define("SOYCMS_BUILD","2016-12-16T15:16:55+09:00");
-define("SOYCMS_BUILD_TIME","1481869016");
+define("SOYCMS_VERSION","1.8.12p7.37");
+define("SOYCMS_BUILD","2016-12-19T15:56:21+09:00");
+define("SOYCMS_BUILD_TIME","1482130582");
 define("SOYCMS_REVISION","42546");
 //define("SOYCMS_ASP_MODE", "develop");
