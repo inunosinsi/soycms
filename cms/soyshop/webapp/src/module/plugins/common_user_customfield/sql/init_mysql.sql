76091ea1a58e6c1841a4897861fa71cc65836dd1
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-02-03 05:28:57 +0900

    ユーザカスタムフィールドのデータベーススキーマでuser_idの制約にNOT NULLを追加

--- cms/soyshop/webapp/src/module/plugins/common_user_customfield/sql/init_mysql.sql
+++ cms/soyshop/webapp/src/module/plugins/common_user_customfield/sql/init_mysql.sql
@@ -1,5 +1,5 @@
 create table soyshop_user_attribute(
-	user_id integer,
+	user_id INTEGER NOT NULL,
 	user_field_id VARCHAR(255),
 	user_value TEXT,
 	unique(user_id,user_field_id)
--- cms/soyshop/webapp/src/module/plugins/common_user_customfield/sql/init_sqlite.sql
+++ cms/soyshop/webapp/src/module/plugins/common_user_customfield/sql/init_sqlite.sql
@@ -1,5 +1,5 @@
 create table soyshop_user_attribute(
-	user_id integer,
+	user_id INTEGER NOT NULL,
 	user_field_id varchar,
 	user_value varchar,
 	unique(user_id,user_field_id)
