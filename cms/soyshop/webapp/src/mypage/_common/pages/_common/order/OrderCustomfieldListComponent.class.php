5ac752153a71fd618b92ecbf3fb50e90c853d41a
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-02-14 07:07:33 +0900

    マイページの注文詳細でリンク付きのオーダーカスタムフィールドの値を使用できるようにした

--- cms/soyshop/webapp/src/mypage/_common/pages/_common/order/OrderCustomfieldListComponent.class.php
+++ cms/soyshop/webapp/src/mypage/_common/pages/_common/order/OrderCustomfieldListComponent.class.php
@@ -8,9 +8,18 @@ class OrderCustomfieldListComponent extends HTMLList {
 			"text" => (isset($item["name"])) ? $item["name"] : ""
 		));
 
+		$val = (isset($item["value"])) ? nl2br(htmlspecialchars($item["value"], ENT_QUOTES, "UTF-8"))  : "";
+		if(isset($item["style"])){
+			$val = "<span style=\"" . $item["style"] . "\">" . $val . "</span>";
+		}
+		
+		if(isset($item["link"])){
+			$val = "<a href=\"" . $item["link"] . "\" target=\"_blank\">" . $val . "</a>";
+		}
+
 		$this->addLabel("customfield_value", array(
-			"html" => (isset($item["value"])) ? nl2br(htmlspecialchars($item["value"], ENT_QUOTES, "UTF-8")) : ""
-		));		
+			"html" => $val
+		));
 	}
 }
 ?>
