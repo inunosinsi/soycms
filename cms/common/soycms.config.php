85f3b19c56c850ff1574a87db86cbd4ffe48aa69
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-03-07 14:09:31 +0900

    記事投稿画面のカテゴリ分けで使用するラベルを一括で外すボタンを追加

--- cms/common/soycms.config.php
+++ cms/common/soycms.config.php
@@ -1,8 +1,8 @@
 <?php
 define("SOYCMS_DB_TYPE","mysql");
 //define("SOY2HTML_CACHE_FORCE",false);
-define("SOYCMS_VERSION","1.8.12p7.45");
-define("SOYCMS_BUILD","2017-03-07T06:57:18+09:00");
-define("SOYCMS_BUILD_TIME","1488837439");
+define("SOYCMS_VERSION","1.8.12p7.46");
+define("SOYCMS_BUILD","2017-03-07T14:07:20+09:00");
+define("SOYCMS_BUILD_TIME","1488863241");
 define("SOYCMS_REVISION","42546");
 //define("SOYCMS_ASP_MODE", "develop");
