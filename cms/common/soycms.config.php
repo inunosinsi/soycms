d110a2c15f264f12c9966509464b311f482712a3
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-03-24 15:15:42 +0900

    TinyMCEの画像アップロード画面のスマホ対応

--- cms/common/soycms.config.php
+++ cms/common/soycms.config.php
@@ -1,8 +1,8 @@
 <?php
 define("SOYCMS_DB_TYPE","mysql");
 //define("SOY2HTML_CACHE_FORCE",false);
-define("SOYCMS_VERSION","1.8.12p7.48");
-define("SOYCMS_BUILD","2017-03-24T04:44:41+09:00");
-define("SOYCMS_BUILD_TIME","1490298282");
+define("SOYCMS_VERSION","1.8.12p7.49");
+define("SOYCMS_BUILD","2017-03-24T15:12:25+09:00");
+define("SOYCMS_BUILD_TIME","1490335945");
 define("SOYCMS_REVISION","42546");
 //define("SOYCMS_ASP_MODE", "develop");
