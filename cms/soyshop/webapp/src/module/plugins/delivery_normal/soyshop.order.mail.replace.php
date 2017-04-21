5e72f5dde4d73c84b6217a45617002264e371778
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-04-22 05:41:33 +0900

    メールのテンプレートの置換文字列に#DELIVERY_DATE_AUTO#を追加

--- cms/soyshop/webapp/src/module/plugins/delivery_normal/soyshop.order.mail.replace.php
+++ cms/soyshop/webapp/src/module/plugins/delivery_normal/soyshop.order.mail.replace.php
@@ -3,12 +3,21 @@
 class DeliveryNormalMailReplace extends SOYShopOrderMailReplace{
 
 	function strings(){
-		return array(
-			"DELIVERY_METHOD" => "配送方法",
-			"DELIVERY_DATE" => "お届け日",
-			"DELIVERY_TIME" => "お届け時間",
-			"POSTAGE" => "送料"
-		);
+		$strings = array();
+		$strings["DELIVERY_METHOD"] = "配送方法";
+		$strings["DELIVERY_DATE"] = "お届け日";
+		
+		SOY2::import("module.plugins.delivery_normal.util.DeliveryNormalUtil");
+		$conf = DeliveryNormalUtil::getDeliveryDateConfig();
+		
+		if(isset($conf["delivery_date_mail_insert_date"]) && (int)$conf["delivery_date_mail_insert_date"] > 0){
+			$strings["DELIVERY_DATE_AUTO"] = "お届け日(自動)";
+		}
+		
+		$strings["DELIVERY_TIME"] = "お届け時間";
+		$strings["POSTAGE"] = "送料";
+		
+		return $strings;
 	}
 
 	function replace(SOYShop_Order $order, $content){
@@ -20,7 +29,14 @@ class DeliveryNormalMailReplace extends SOYShopOrderMailReplace{
 		
 		//お届け日
 		$attr = (isset($list["delivery_normal.date"])) ? $list["delivery_normal.date"] : array();
-		$content = str_replace("#DELIVERY_DATE#", self::getDeliveryDate($attr), $content);
+		$content = str_replace("#DELIVERY_DATE#", $attr["value"], $content);
+		
+		//お届け日(自動)
+		SOY2::import("module.plugins.delivery_normal.util.DeliveryNormalUtil");
+		$conf = DeliveryNormalUtil::getDeliveryDateConfig();
+		if(isset($conf["delivery_date_mail_insert_date"]) && (int)$conf["delivery_date_mail_insert_date"] > 0){
+			$content = str_replace("#DELIVERY_DATE_AUTO#", date("Y-m-d", time() + $conf["delivery_date_mail_insert_date"] * 24 * 60 * 60), $content);
+		}
 		
 		//お届け時間
 		$time = (isset($list["delivery_normal.time"])) ? $list["delivery_normal.time"]["value"] : "";
@@ -32,24 +48,6 @@ class DeliveryNormalMailReplace extends SOYShopOrderMailReplace{
 		$content = str_replace("#POSTAGE#", $postage, $content);
 		
 		return $content;
-	}
-		
-	private function getDeliveryDate($attr){
-		if(!isset($attr["value"])) return "";
-		
-		if($attr["value"] == "指定なし"){
-			
-			//発送メールとその他のメール以外は指定なしで送る
-			if(!isset($_GET["type"]) || $_GET["type"] == "order" || $_GET["type"] == "confirm" || $_GET["type"] == "payment") return $attr["value"];
-			
-			SOY2::import("module.plugins.delivery_normal.util.DeliveryNormalUtil");
-			$conf = DeliveryNormalUtil::getDeliveryDateConfig();
-			if(isset($conf["delivery_date_mail_insert_date"]) && (int)$conf["delivery_date_mail_insert_date"] > 0){
-				return date("Y-m-d", time() + $conf["delivery_date_mail_insert_date"] * 24 * 60 * 60);
-			}
-		}
-		
-		return $attr["value"];
 	}
 }
 
