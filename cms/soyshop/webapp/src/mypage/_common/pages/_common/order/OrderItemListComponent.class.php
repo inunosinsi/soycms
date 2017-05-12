a49aafc62cc721886b553b52359640ed4bd5eb45
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-05-13 04:59:14 +0900

    マイページの商品一覧でsoy:id="item_small_image"を追加

--- cms/soyshop/webapp/src/mypage/_common/pages/_common/order/OrderItemListComponent.class.php
+++ cms/soyshop/webapp/src/mypage/_common/pages/_common/order/OrderItemListComponent.class.php
@@ -1,44 +1,47 @@
 <?php
 
 class OrderItemListComponent extends HTMLList{
-	
-	private $itemDao;
-	
-	function populateItem($entity){
-		$item = $this->getItem($entity);
-		
-		$this->addLink("item_link", array(
-			"link" => soyshop_get_item_detail_link($item)
-		));
 
-		$this->addLabel("item_name", array(
-			"text" => (strlen($item->getCode())) ? $entity->getItemName() : "---"
-		));
-		$delegate = SOYShopPlugin::invoke("soyshop.item.option", array(
-			"mode" => "display",
-			"item" => $entity,
-		));
-		
-		$this->addModel("has_option", array(
-			"visible" => (strlen($delegate->getHtmls()) > 0)
-		));
+    private $itemDao;
 
-		$this->addLabel("item_option", array(
-			"html" => $delegate->getHtmls()
-		));
-	}
-	
-	function getItem($entity){
-		if(!$this->itemDao){
-			$this->itemDao = SOY2DAOFactory::create("shop.SOYShop_ItemDAO");
-		}
-		
-		try{
-			$item = $this->itemDao->getById($entity->getItemId());
-		}catch(Exception $e){
-			$item = new SOYShop_Item();
-		}
-		return $item;
-	}
+    function populateItem($entity){
+        $item = $this->getItem($entity);
+
+        $this->addLink("item_link", array(
+            "link" => soyshop_get_item_detail_link($item)
+        ));
+
+        $this->addLabel("item_name", array(
+            "text" => (strlen($item->getCode())) ? $entity->getItemName() : "---"
+        ));
+        $delegate = SOYShopPlugin::invoke("soyshop.item.option", array(
+            "mode" => "display",
+            "item" => $entity,
+        ));
+
+        $this->addImage("item_small_image", array(
+            "src" => soyshop_convert_file_path($item->getAttribute("image_small"), $item)
+        ));
+
+        $this->addModel("has_option", array(
+            "visible" => (strlen($delegate->getHtmls()) > 0)
+        ));
+
+        $this->addLabel("item_option", array(
+            "html" => $delegate->getHtmls()
+        ));
+    }
+
+    function getItem($entity){
+        if(!$this->itemDao){
+            $this->itemDao = SOY2DAOFactory::create("shop.SOYShop_ItemDAO");
+        }
+
+        try{
+            $item = $this->itemDao->getById($entity->getItemId());
+        }catch(Exception $e){
+            $item = new SOYShop_Item();
+        }
+        return $item;
+    }
 }
-?>
