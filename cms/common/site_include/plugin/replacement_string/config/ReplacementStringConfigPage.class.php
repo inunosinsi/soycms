96521f309f510cf78ee04b87fc2feedbed128d60
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-08-01 15:16:00 +0900

    置換文字列生成プラグインの更新の処理内にvar_dumpが残っていたので修正

--- cms/common/site_include/plugin/replacement_string/config/ReplacementStringConfigPage.class.php
+++ cms/common/site_include/plugin/replacement_string/config/ReplacementStringConfigPage.class.php
@@ -1,74 +1,72 @@
 <?php
 
 class ReplacementStringConfigPage extends WebPage{
-	
+
 	private $pluginObj;
-	
+
 	function __construct(){}
-	
+
 	function doPost(){
-		
+
 		if(soy2_check_token()){
-			
+
 			$list = $this->pluginObj->getStringList();
-			
+
 			if(isset($_POST["add"])){
-								
+
 				$values = array();
 				$values["symbol"] = trim(htmlspecialchars($_POST["symbol"], ENT_QUOTES, "UTF-8"));
 				$values["string"] = trim(htmlspecialchars($_POST["string"], ENT_QUOTES, "UTF-8"));
-				
+
 				$list[] = $values;
 				$this->pluginObj->setStringList($list);
-				
+
 				CMSPlugin::savePluginConfig($this->pluginObj->getId(), $this->pluginObj);
 				CMSPlugin::redirectConfigPage();
 			}
-			
+
 			if(isset($_POST["change"])){
 				foreach($list as $key => $values){
-					var_dump($key);
-					
 					if(isset($_POST["string"][$key])){
 						$values["string"] = trim(htmlspecialchars($_POST["string"][$key], ENT_QUOTES, "UTF-8"));
 					}
-					
+
 					$list[$key] = $values;
 				}
-				
+
 				$this->pluginObj->setStringList($list);
-				
+
 				CMSPlugin::savePluginConfig($this->pluginObj->getId(), $this->pluginObj);
 				CMSPlugin::redirectConfigPage();
 			}
 		}
 	}
-	
+
 	function execute(){
 		//削除
 		if(isset($_GET["remove"])){
 			self::remove();
 		}
-		
+
 		if(method_exists("WebPage", "WebPage")){
 			WebPage::WebPage();
 		}else{
 			WebPage::__construct();
 		}
-		
+
 		$this->addForm("form");
-		
+
 		$list = $this->pluginObj->getStringList();
 		DisplayPlugin::toggle("has_symbol_list", count($list));
-		
+
 		$this->addForm("change_form");
-		
+
 		SOY2::import("site_include.plugin.replacement_string.component.ReplacementStringListComponent");
 		$this->createAdd("string_list", "ReplacementStringListComponent", array(
 			"list" => $list
 		));
 	}
-	
+
 	private function remove(){
 		$list = $this->pluginObj->getStringList();
 		if(isset($list[$_GET["remove"]])){
@@ -80,16 +78,15 @@ class ReplacementStringConfigPage extends WebPage{
 					$array[] = $values;
 				}
 			}
-			
+
 			$this->pluginObj->setStringList($array);
 			CMSPlugin::savePluginConfig($this->pluginObj->getId(), $this->pluginObj);
 			header("Location:" . SOY2PageController::createRelativeLink($_SERVER["PHP_SELF"], true) . "?replacement_string#config");
 			exit;
 		}
 	}
-	
+
 	function setPluginObj($pluginObj){
 		$this->pluginObj = $pluginObj;
 	}
 }
-?>
