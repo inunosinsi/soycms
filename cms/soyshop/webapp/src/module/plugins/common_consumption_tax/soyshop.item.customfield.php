52fa3d47d0e16d5831d31b4c69b10b3814470b84
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-08-04 19:55:55 +0900

    外税計算の際に価格に数字以外の値が登録されている場合にエラーになる件を修正

--- cms/soyshop/webapp/src/module/plugins/common_consumption_tax/soyshop.item.customfield.php
+++ cms/soyshop/webapp/src/module/plugins/common_consumption_tax/soyshop.item.customfield.php
@@ -12,21 +12,22 @@ class CommonConsumptionTaxCustomField extends SOYShopItemCustomFieldBase{
 	 * onOutput
 	 */
 	function onOutput($htmlObj, SOYShop_Item $item){
-		
+
 		$taxRate = $this->getTaxRate();
-		
+
 		//0:表示価格 1:通常価格 2:セール価格
 		$prices = array(
 			$item->getSellingPrice(),
 			$item->getPrice(),
 			$item->getSalePrice()
 		);
-		
+
 		if(isset($taxRate)){
 			SOY2::import("module.plugins.common_consumption_tax.util.ConsumptionTaxUtil");
 			$config = ConsumptionTaxUtil::getConfig();
 			$m = (isset($config["method"])) ? $config["method"] : 0;
 			for($i = 0; $i < count($prices); $i++){
+				$prices[$i] = (isset($prices[$i]) && is_numeric($prices[$i])) ? (int)$prices[$i] : 0;
 				switch($m){
 					case ConsumptionTaxUtil::METHOD_ROUND:
 						$postTaxPrice = round($prices[$i] * $taxRate / 100);
@@ -38,22 +39,22 @@ class CommonConsumptionTaxCustomField extends SOYShopItemCustomFieldBase{
 					default:
 						$postTaxPrice = floor($prices[$i] * $taxRate / 100);
 				}
-				
+
 				$prices[$i] += $postTaxPrice;
 			}
-				
+
 		}
-		
+
 		$htmlObj->addLabel("post_tax_price", array(
 			"soy2prefix" => SOYSHOP_SITE_PREFIX,
 			"text" => number_format($prices[0])
 		));
-		
+
 		$htmlObj->addLabel("post_tax_normal_price", array(
 			"soy2prefix" => SOYSHOP_SITE_PREFIX,
 			"text" => number_format($prices[1])
 		));
-		
+
 		$htmlObj->addLabel("post_tax_sale_price", array(
 			"soy2prefix" => SOYSHOP_SITE_PREFIX,
 			"text" => number_format($prices[2])
@@ -61,7 +62,7 @@ class CommonConsumptionTaxCustomField extends SOYShopItemCustomFieldBase{
 	}
 
 	function onDelete($id){}
-	
+
 	function getTaxRate(){
 		if(is_null($this->taxRate)){
 			SOY2::import("domain.config.SOYShop_ShopConfig");
@@ -70,22 +71,22 @@ class CommonConsumptionTaxCustomField extends SOYShopItemCustomFieldBase{
 				SOY2::imports("module.plugins.common_consumption_tax.domain.*");
 				$scheduleDao = SOY2DAOFactory::create("SOYShop_ConsumptionTaxScheduleDAO");
 				$scheduleDao->setLimit(1);
-				
+
 				try{
 					$schedules = $scheduleDao->getScheduleByDate(time());
 				}catch(Exception $e){
 					$schedules = array();
 				}
-				
+
 				if(isset($schedules[0])){
 					$this->taxRate = (int)$schedules[0]->getTaxRate();
 					return $this->taxRate;
 				}
 			}
-			
-			$this->taxRate = 0;	
+
+			$this->taxRate = 0;
 		}
-		
+
 		return $this->taxRate;
 	}
 }
