14a45795c5611ba872eb1767e1aa12d2be99262d
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-08-19 07:34:21 +0900

    画像URL変換プラグイン

---
+++ cms/common/site_include/plugin/convert_image_url/config/ConvertImageUrlConfigPage.class.php
@@ -0,0 +1,54 @@
+<?php
+
+class ConvertImageUrlConfigPage extends WebPage{
+
+  private $pluginObj;
+
+  function __construct(){}
+
+  function doPost(){
+    if(soy2_check_token()){
+      //SQLite版の場合はバックアップをとる
+      if(SOYCMS_DB_TYPE == "sqlite"){
+        $backupDir = UserInfoUtil::getSiteDirectory() . ".backup/";
+        if(!file_exists($backupDir)) mkdir($backupDir);
+        $backupDir = $backupDir . date("Ymd") . "/";
+        if(!file_exists($backupDir)) mkdir($backupDir);
+
+        $dbFilePath = UserInfoUtil::getSiteDirectory() . ".db/sqlite.db";
+        copy($dbFilePath, $backupDir . "sqlite.db");
+      }
+
+      $dao = new SOY2DAO();
+
+      $domain = $_SERVER["HTTP_HOST"];
+      foreach(array("content", "more") as $col){
+        $sql = "UPDATE Entry SET " . $col  . "=REPLACE(" . $col . ", 'src=\"http://" . $domain . "', 'src=\"');";
+        try{
+          $dao->executeUpdateQuery($sql);
+        }catch(Exception $e){
+          var_dump($e);
+        }
+      }
+
+      CMSPlugin::redirectConfigPage();
+    }
+  }
+
+  function execute(){
+    if(method_exists("WebPage", "WebPage")){
+			WebPage::WebPage();
+		}else{
+			WebPage::__construct();
+		}
+
+    DisplayPlugin::toggle("mode_sqlite", (SOYCMS_DB_TYPE == "sqlite"));
+    DisplayPlugin::toggle("mode_mysql", (SOYCMS_DB_TYPE == "mysql"));
+
+    $this->addForm("form");
+  }
+
+  function setPluginObj($pluginObj){
+    $this->pluginObj = $pluginObj;
+  }
+}
---
+++ cms/common/site_include/plugin/convert_image_url/convert_image_url.php
@@ -0,0 +1,50 @@
+<?php
+ConvertImageUrlPlugin::registerPlugin();
+
+class ConvertImageUrlPlugin{
+
+	const PLUGIN_ID = "convert_image_url";
+
+	function getId(){
+		return self::PLUGIN_ID;
+	}
+
+	function init(){
+		CMSPlugin::addPluginMenu($this->getId(),array(
+			"name"=> "画像URL変換プラグイン",
+			"description"=> "SSL対応時にブログで投稿した画像のパスがhttpが始まるため、/からはじまる絶対パスに変換する",
+			"author"=> "齋藤毅",
+			"url"=> "https://saitodev.co",
+			"mail"=>"tsuyoshi@saitodev.co",
+			"version"=>"0.1"
+		));
+		
+		if(CMSPlugin::activeCheck($this->getId())){
+
+			CMSPlugin::addPluginConfigPage($this->getId(),array(
+				$this,"config_page"
+			));
+		}
+	}
+
+	function config_page($message){
+		SOY2::import("site_include.plugin.convert_image_url.config.ConvertImageUrlConfigPage");
+		$form = SOY2HTMLFactory::createInstance("ConvertImageUrlConfigPage");
+		$form->setPluginObj($this);
+		$form->execute();
+		return $form->getObject();
+	}
+
+	/**
+	 * プラグインの登録
+	 */
+	public static function registerPlugin(){
+
+		$obj = CMSPlugin::loadPluginConfig(self::PLUGIN_ID);
+		if(is_null($obj)){
+			$obj = new ConvertImageUrlPlugin();
+		}
+
+		CMSPlugin::addPlugin(self::PLUGIN_ID,array($obj,"init"));
+	}
+}
