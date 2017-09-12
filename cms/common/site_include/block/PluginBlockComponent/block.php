eaeb6984cb80b9787af46a0460f1b3bc1e53f992
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-09-12 15:33:54 +0900

    プラグインブロックの設定画面のUI修正

--- cms/common/site_include/block/PluginBlockComponent/block.php
+++ cms/common/site_include/block/PluginBlockComponent/block.php
@@ -7,13 +7,13 @@ class PluginBlockComponent implements BlockComponent{
     private $pluginId;
 	private $isStickUrl = false;
 	private $blogPageId;
-    
+
 	/**
 	 * @return SOY2HTML
 	 * 設定画面用のHTMLPageComponent
 	 */
 	function getFormPage(){
-        
+
         $pluginIds = array();
         $onLoad = CMSPlugin::getEvent('onPluginBlockAdminReturnPluginId');
 		foreach($onLoad as $plugin){
@@ -21,14 +21,14 @@ class PluginBlockComponent implements BlockComponent{
             $res = call_user_func($func);
             if(isset($res) && strlen($res)) $pluginIds[] = htmlspecialchars($res, ENT_QUOTES, "UTF-8");
 		}
-        
+
 		return SOY2HTMLFactory::createInstance("PluginBlockComponent_FormPage",array(
 			"entity" => $this,
 			"blogPages" => SOY2Logic::createInstance("logic.site.Page.BlogPageLogic")->getBlogPageList(),//ブログ一覧を取得する
             "pluginIds" => $pluginIds
 		));
 	}
-    
+
 	/**
 	 * @return SOY2HTML
 	 * 表示用コンポーネント
@@ -61,7 +61,7 @@ class PluginBlockComponent implements BlockComponent{
 				$this->isStickUrl = false;
 			}
 		}
-        
+
 		return SOY2HTMLFactory::createInstance("PluginBlockComponent_ViewPage",array(
 			"list" => $array,
 			"isStickUrl" => $this->isStickUrl,
@@ -69,9 +69,9 @@ class PluginBlockComponent implements BlockComponent{
 			"blogPageId"=>$this->blogPageId,
 			"soy2prefix"=>"block",
 		));
-        
+
 	}
-    
+
 	/**
 	 * @return string
 	 * 一覧表示に出力する文字列
@@ -84,14 +84,14 @@ class PluginBlockComponent implements BlockComponent{
             return "設定なし";
         }
 	}
-    
+
 	/**
 	 * @return string コンポーネント名
 	 */
 	function getComponentName(){
 		return CMSMessageManager::get("SOYCMS_PLUGIN_BLOCK");
 	}
-    
+
 	function getComponentDescription(){
 		return CMSMessageManager::get("SOYCMS_PLUGIN_BLOCK_DESCRIPTION");
 	}
@@ -119,23 +119,23 @@ class PluginBlockComponent implements BlockComponent{
 
 
 class PluginBlockComponent_FormPage extends HTMLPage{
-    
+
 	private $entity;
 	private $blogPages = array();
     private $pluginIds = array();
-    
+
 	function __construct(){
 		parent::__construct();
-        
+
 	}
-    
+
 	function execute(){
-                        
+
 		$this->createAdd("no_stick_url","HTMLHidden",array(
 			"name" => "object[isStickUrl]",
 			"value" => 0,
 		));
-        
+
 		$this->addCheckBox("stick_url", array(
 			"name" => "object[isStickUrl]",
 			"label" => CMSMessageManager::get("SOYCMS_BLOCK_ADD_ENTRY_LINK_TO_THE_TITLE"),
@@ -143,10 +143,10 @@ class PluginBlockComponent_FormPage extends HTMLPage{
 			"selected" => $this->entity->getIsStickUrl(),
 			"visible" =>  (count($this->blogPages) > 0)
 		));
-        
+
 		$style = SOY2HTMLFactory::createInstance("SOY2HTMLStyle");
 		$style->display = ($this->entity->getIsStickUrl()) ? "" : "none";
-        
+
 		$this->addSelect("blog_page_list", array(
 			"name" => "object[blogPageId]",
 			"selected" => $this->entity->getBlogPageId(),
@@ -154,7 +154,7 @@ class PluginBlockComponent_FormPage extends HTMLPage{
 			"visible" => (count($this->blogPages) > 0),
 			"style" => $style
 		));
-        
+
 		$this->addLabel("blog_page_list_label", array(
 			"text" => CMSMessageManager::get("SOYCMS_BLOCK_SELECT_BLOG_TITLE"),
 			"visible" => (count($this->blogPages) > 0),
@@ -167,17 +167,21 @@ class PluginBlockComponent_FormPage extends HTMLPage{
             "options" => $this->pluginIds,
             "visible" => (count($this->pluginIds) > 0)
         ));
-        
-		$this->addForm("main_form", array());        
+
+		$this->addForm("main_form", array());
+
+		if(count($this->blogPages) === 0){
+			DisplayPlugin::hide("blog_link");
+		}      
 	}
-    
+
 	/**
 	 * ラベル表示コンポーネントの実装を行う
 	 */
 	function setEntity(PluginBlockComponent $block){
 		$this->entity = $block;
 	}
-    
+
 	/**
 	 * ブログページを渡す
 	 *
@@ -190,7 +194,7 @@ class PluginBlockComponent_FormPage extends HTMLPage{
     function setPluginIds($pluginIds){
         $this->pluginIds = $pluginIds;
     }
-    
+
     /**
      *  ラベルオブジェクトのリストを返す
      *  NOTE:個数に考慮していない。ラベルの量が多くなるとpagerの実装が必要？
@@ -199,7 +203,7 @@ class PluginBlockComponent_FormPage extends HTMLPage{
     	$dao = SOY2DAOFactory::create("cms.LabelDAO");
     	return $dao->get();
     }
-    
+
 	function getTemplateFilePath(){
 		if(!defined("SOYCMS_LANGUAGE")||SOYCMS_LANGUAGE=="ja"||!file_exists(CMS_BLOCK_DIRECTORY . "PluginBlockComponent" . "/form_".SOYCMS_LANGUAGE.".html")){
             return CMS_BLOCK_DIRECTORY . "PluginBlockComponent" . "/form.html";
@@ -211,50 +215,50 @@ class PluginBlockComponent_FormPage extends HTMLPage{
 
 
 class PluginBlockComponent_ViewPage extends HTMLList{
-    
+
 	var $isStickUrl;
 	var $articlePageUrl;
 	var $blogPageId;
-    
-    
+
+
 	function setIsStickUrl($flag){
 		$this->isStickUrl = $flag;
 	}
-    
+
 	function setArticlePageUrl($articlePageUrl){
 		$this->articlePageUrl = $articlePageUrl;
 	}
-    
+
 	function setBlogPageId($id){
 		$this->blogPageId = $id;
 	}
-    
+
 	function getStartTag(){
-        
+
 		return parent::getStartTag();
 	}
-    
+
 	/**
 	 * 実行前後にDSNの書き換えを実行
 	 */
 	function execute(){
 		parent::execute();
 	}
-    
+
 	function populateItem($entity){
-        
+
 		$hTitle = htmlspecialchars($entity->getTitle(), ENT_QUOTES, "UTF-8");
 		$entryUrl = $this->articlePageUrl.rawurlencode($entity->getAlias());
-        
+
 		if($this->isStickUrl){
 			$hTitle = "<a href=\"".htmlspecialchars($entryUrl, ENT_QUOTES, "UTF-8")."\">".$hTitle."</a>";
 		}
-        
+
 		$this->createAdd("entry_id","CMSLabel",array(
 			"text"=> $entity->getId(),
 			"soy2prefix"=>"cms"
 		));
-        
+
 		$this->createAdd("title","CMSLabel",array(
 			"html"=> $hTitle,
 			"soy2prefix"=>"cms"
@@ -271,50 +275,50 @@ class PluginBlockComponent_ViewPage extends HTMLList{
 			"text"=>$entity->getCdate(),
 			"soy2prefix"=>"cms"
 		));
-        
+
 		$this->createAdd("create_time","DateLabel",array(
 			"text"=>$entity->getCdate(),
 			"soy2prefix"=>"cms",
 			"defaultFormat"=>"H:i"
 		));
-        
+
 		//entry_link追加
 		$this->createAdd("entry_link","HTMLLink",array(
 			"link" => $entryUrl,
 			"soy2prefix"=>"cms"
 		));
-        
+
 		//リンクの付かないタイトル 1.2.6～
 		$this->createAdd("title_plain","CMSLabel",array(
 			"text"=> $entity->getTitle(),
 			"soy2prefix"=>"cms"
 		));
-        
+
 		//1.2.7～
 		$this->createAdd("more_link","HTMLLink",array(
 			"soy2prefix"=>"cms",
 			"link" => $entryUrl ."#more",
 			"visible"=>(strlen($entity->getMore()) != 0)
 		));
-        
+
 		//1.7.5~
 		$this->createAdd("update_date","DateLabel",array(
 			"text"=>$entity->getUdate(),
 			"soy2prefix"=>"cms",
 		));
-        
+
 		$this->createAdd("update_time","DateLabel",array(
 			"text"=>$entity->getUdate(),
 			"soy2prefix"=>"cms",
 			"defaultFormat"=>"H:i"
 		));
-        
+
 		$this->createAdd("entry_url","HTMLLabel",array(
 			"text"=>$entryUrl,
 			"soy2prefix"=>"cms",
 		));
 
 		CMSPlugin::callEventFunc('onEntryOutput',array("entryId"=>$entity->getId(),"SOY2HTMLObject"=>$this,"entry"=>$entity));
-	}    
+	}
 }
 ?>
