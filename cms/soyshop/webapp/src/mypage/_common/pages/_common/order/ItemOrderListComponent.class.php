d1dc6de574276b5d9e7fac28323c0c7b3eab3656
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-12-01 09:42:27 +0900

    マイページの注文履歴一覧で親商品の画像を参照できるようにした

--- cms/soyshop/webapp/src/mypage/_common/pages/_common/order/ItemOrderListComponent.class.php
+++ cms/soyshop/webapp/src/mypage/_common/pages/_common/order/ItemOrderListComponent.class.php
@@ -6,8 +6,8 @@ class ItemOrderListComponent extends HTMLList{
 
 	protected function populateItem($itemOrder) {
 
-		$item = $this->getItem($itemOrder->getItemId());
-		
+		$item = self::getItem($itemOrder->getItemId());
+
 		$this->addLink("item_link", array(
 			"link" => soyshop_get_item_detail_link($item)
 		));
@@ -15,11 +15,11 @@ class ItemOrderListComponent extends HTMLList{
 		$this->addLink("item_code", array(
 			"text" => (strlen($item->getCode()) > 0) ? $item->getCode() : "deleted item " . $itemOrder->getItemId(),
 		));
-		
+
 		$this->addLabel("item_code_plain", array(
 			"text" => (strlen($item->getCode()) > 0) ? $item->getCode() : "deleted item " . $itemOrder->getItemId()
 		));
-		
+
 		$this->addImage("item_small_image", array(
 			"src" => soyshop_convert_file_path($item->getAttribute("image_small"), $item)
 		));
@@ -27,7 +27,7 @@ class ItemOrderListComponent extends HTMLList{
 		$this->addLabel("item_name", array(
 			"text" => (strlen($item->getCode())) ? $itemOrder->getItemName() : "---"
 		));
-		
+
 		$delegate = SOYShopPlugin::invoke("soyshop.item.option", array(
 			"mode" => "display",
 			"item" => $itemOrder,
@@ -48,22 +48,57 @@ class ItemOrderListComponent extends HTMLList{
 		$this->addLabel("item_total_price", array(
 			"text" => number_format($itemOrder->getTotalPrice())
 		));
+
+		//子商品
+		if(is_numeric($item->getType())){
+			$parent = self::getParentItem($item->getType());
+		}else{
+			$parent = new SOYShop_Item();
+		}
+
+		/** 親商品関連のタグ **/
+		$this->addLink("parent_link", array(
+			"text" => $parent->getOpenItemName(),
+			"link" => soyshop_get_item_detail_link($parent)
+		));
+		$this->addLabel("parent_name_plain", array(
+			"text" => $parent->getOpenItemName(),
+		));
+
+		$this->addLabel("parent_code", array(
+			"text" => $parent->getCode(),
+		));
+
+		$this->addImage("parent_small_image", array(
+			"src" => soyshop_convert_file_path($parent->getAttribute("image_small"), $parent)
+		));
+
+		$this->addImage("parent_large_image", array(
+			"src" => soyshop_convert_file_path($parent->getAttribute("image_large"), $parent)
+		));
 	}
 
-	/**
-	 * @return object#SOYShop_Item
-	 * @param itemId
-	 */
-	function getItem($itemId){
+	private function getParentItem($itemId){
 		try{
 			return $this->itemDao->getById($itemId);
 		}catch(Exception $e){
 			return new SOYShop_Item();
 		}
 	}
-	
+
+	/**
+	 * @return object#SOYShop_Item
+	 * @param itemId
+	 */
+	private function getItem($itemId){
+		try{
+			return $this->itemDao->getById($itemId);
+		}catch(Exception $e){
+			return new SOYShop_Item();
+		}
+	}
+
 	function setItemDao($itemDao){
 		$this->itemDao = $itemDao;
 	}
 }
-?>
