5a77ef4a38e2babf202b20b47a6c6a8ab2576919
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-12-16 05:08:42 +0900

    elfinderでPDF,epubとzipファイルを許可

--- cms/soycms/js/elfinder/php/connector.php
+++ cms/soycms/js/elfinder/php/connector.php
@@ -146,7 +146,7 @@ $opts = array(
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-			'uploadAllow'   => array('image', 'text/plain'),// Mimetype `image` and `text/plain` allowed to upload
+			'uploadAllow'   => array('image', 'text/plain', 'text/css', 'application/zip', 'application/epub+zip','application/pdf'),// Mimetype `image` and `text/plain` allowed to upload
 			'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
 			'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
 		),
