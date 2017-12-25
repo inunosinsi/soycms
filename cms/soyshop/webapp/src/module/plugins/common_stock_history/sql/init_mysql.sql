34683afbecc7da7c40a8a67f84194f7a953af8d1
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2017-12-26 06:38:20 +0900

    在庫管理系のプラグインのリファクタリング

--- cms/soyshop/webapp/src/module/plugins/common_stock_history/sql/init_mysql.sql
+++ cms/soyshop/webapp/src/module/plugins/common_stock_history/sql/init_mysql.sql
@@ -2,4 +2,4 @@ create table soyshop_stock_history(
 	item_id integer not null,
 	memo varchar(512),
 	create_date integer not null
-)ENGINE = Archive;
+)ENGINE = InnoDB;
--- cms/soyshop/webapp/src/module/plugins/item_stock_manager/soyshop.item.update.php
+++ cms/soyshop/webapp/src/module/plugins/item_stock_manager/soyshop.item.update.php
@@ -3,18 +3,17 @@ SOY2::imports("module.plugins.item_stock_manager.domain.*");
 class ItemStockManagerUpdate extends SOYShopItemUpdateBase{
 
 	function addHistory(SOYShop_Item $item, $oldStock){
-		
+
 		$newStock = (isset($_POST["Item"]["stock"])) ? (int)$_POST["Item"]["stock"] : (int)$item->getStock();
 		if($oldStock != $newStock){
 			$logMessage = "在庫数を" . $oldStock."から" . $newStock . "に変更しました";
-			
+
 			$dao = SOY2DAOFactory::create("SOYShop_StockHistoryDAO");
-			
+
 			$obj = new SOYShop_StockHistory();
 			$obj->setItemId($item->getId());
 			$obj->setUpdateStock($newStock - $oldStock);
 			$obj->setMemo($logMessage);
-			$obj->setCreateDate(time());
 			
 			try{
 				$dao->insert($obj);
@@ -23,11 +22,11 @@ class ItemStockManagerUpdate extends SOYShopItemUpdateBase{
 			}
 		}
 	}
-	
+
 	function display(SOYShop_Item $item){
 		$dao = SOY2DAOFactory::create("SOYShop_StockHistoryDAO");
 		$dao->setLimit(5);
-		
+
 		try{
 			return $dao->getByItemId($item->getId());
 		}catch(Exception $e){
