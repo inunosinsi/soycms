550f97cc1ca361a357af6b8e6f5c50fc0f46d246
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-02-19 10:30:17 +0900

    リファクタリング

--- cms/soyshop/webapp/src/module/plugins/common_add_mail_type/soyshop.order.detail.mail.php
+++ cms/soyshop/webapp/src/module/plugins/common_add_mail_type/soyshop.order.detail.mail.php
@@ -1,11 +1,10 @@
 <?php
 class CommonAddMailOrderDetailMail extends SOYShopOrderDetailMailBase{
 
-	
+
 	function getMailType(){
 		SOY2::import("module.plugins.common_add_mail_type.util.AddMailTypeUtil");
 		return AddMailTypeUtil::getConfig();
 	}
 }
 SOYShopPlugin::extension("soyshop.order.detail.mail", "common_add_mail_type", "CommonAddMailOrderDetailMail");
-?>
