da5ee775ba205a4b43da3b712a4bcdf57693e7c2
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-03-06 05:42:42 +0900

    ログイン周りのPHP7対応漏れ

--- cms/admin/webapp/pages/Login/LogoutPage.class.php
+++ cms/admin/webapp/pages/Login/LogoutPage.class.php
@@ -1,11 +1,10 @@
 <?php
 class LogoutPage extends WebPage{
 
-	function LogoutPage() {
+	function __construct() {
 		SOY2::import("action.login.LogoutAction");
 		$action = SOY2ActionFactory::createInstance('LogoutAction');
 		$action->run();
 		SOY2PageController::jump("");
     }
 }
-?>
