f777a5bffb611615458601c339360f40777c0651
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-03-05 10:52:25 +0900

    jsアプリのバージョンアップ周り

--- cms/common/soycms.config.php
+++ cms/common/soycms.config.php
@@ -1,6 +1,6 @@
 <?php
 define("SOYCMS_DB_TYPE","mysql");
-define("SOYCMS_VERSION","3.0.1p45");
+define("SOYCMS_VERSION","3.0.1p46");
 define("SOYCMS_BUILD","2017-11-08T07:00:43+0000");
 define("SOYCMS_BUILD_TIME","1510124446");
 define("SOYCMS_REVISION","45154");
