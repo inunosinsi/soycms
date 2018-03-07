1a7a7c86417f7e73aca7fec1524d80bd87df839c
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-03-07 14:30:34 +0900

    soyshop.admin.top.phpで各メソッドの説明コメントを追加

--- cms/soyshop/webapp/src/logic/plugin/extensions/soyshop.admin.top.php
+++ cms/soyshop/webapp/src/logic/plugin/extensions/soyshop.admin.top.php
@@ -1,21 +1,52 @@
 <?php
 class SOYShopAdminTopBase implements SOY2PluginAction{
-	
-	//ログインしているアカウントの権限によって表示するかどうか？
+
+	/**
+	 * @return boolen
+	 * ログインしているアカウントの権限によって表示するかどうか？
+	 */
 	function allowDisplay(){
 		return true;
 	}
-	function getTitle(){}
-	function getContent(){}
-	function getLink(){}
-	function getLinkTitle(){}
+
+	/**
+	 * @return string
+	 * 新着に項目を追加する際のタイトル部分
+	 */
+	function getTitle(){
+		return "";
+	}
+
+	/**
+	 * @return string
+	 * 新着に項目を追加する際のコンテンツ部分
+	 */
+	function getContent(){
+		return "";
+	}
+
+	/**
+	 * @return string
+	 * タイトル横に表示されるリンクのURL
+	 */
+	function getLink(){
+		return "";
+	}
+
+	/**
+	 * @return string
+	 * タイトル横に表示されるリンクURLのテキスト部分
+	 */
+	function getLinkTitle(){
+		return "";
+	}
 }
 
 class SOYShopAdminTopDeletageAction implements SOY2PluginDelegateAction{
-	
+
 	private $_contents;
-	
-	function run($extetensionId, $moduleId, SOY2PluginAction $action){		
+
+	function run($extetensionId, $moduleId, SOY2PluginAction $action){
 		if($action->allowDisplay()){
 			$array = array();
 			$array["title"] = $action->getTitle();
@@ -25,10 +56,9 @@ class SOYShopAdminTopDeletageAction implements SOY2PluginDelegateAction{
 			$this->_contents[$moduleId] = $array;
 		}
 	}
-	
+
 	function getContents(){
 		return $this->_contents;
 	}
 }
 SOYShopPlugin::registerExtension("soyshop.admin.top", "SOYShopAdminTopDeletageAction");
-?>
