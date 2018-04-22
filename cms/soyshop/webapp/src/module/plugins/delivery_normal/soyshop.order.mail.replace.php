d468f88e210156b8004fc8f8b82d6626c10b68db
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-04-22 20:53:31 +0900

    標準配送モジュールの置換文字列で値が無い場合にnoticeになる件を修正

--- cms/soyshop/webapp/src/module/plugins/delivery_normal/soyshop.order.mail.replace.php
+++ cms/soyshop/webapp/src/module/plugins/delivery_normal/soyshop.order.mail.replace.php
@@ -6,47 +6,47 @@ class DeliveryNormalMailReplace extends SOYShopOrderMailReplace{
 		$strings = array();
 		$strings["DELIVERY_METHOD"] = "配送方法";
 		$strings["DELIVERY_DATE"] = "お届け日";
-		
+
 		SOY2::import("module.plugins.delivery_normal.util.DeliveryNormalUtil");
 		$conf = DeliveryNormalUtil::getDeliveryDateConfig();
-		
+
 		if(isset($conf["delivery_date_mail_insert_date"]) && (int)$conf["delivery_date_mail_insert_date"] > 0){
 			$strings["DELIVERY_DATE_AUTO"] = "お届け日(自動)";
 		}
-		
+
 		$strings["DELIVERY_TIME"] = "お届け時間";
 		$strings["POSTAGE"] = "送料";
-		
+
 		return $strings;
 	}
 
 	function replace(SOYShop_Order $order, $content){
 		$list = $order->getAttributeList();
-		
+
 		//配送方法
 		$method = (isset($list["delivery_normal"])) ? $list["delivery_normal"]["value"] : "";
 		$content = str_replace("#DELIVERY_METHOD#", $method, $content);
-		
+
 		//お届け日
-		$attr = (isset($list["delivery_normal.date"])) ? $list["delivery_normal.date"] : array();
+		$attr = (isset($list["delivery_normal.date"])) ? $list["delivery_normal.date"] : array("value" => "");
 		$content = str_replace("#DELIVERY_DATE#", $attr["value"], $content);
-		
+
 		//お届け日(自動)
 		SOY2::import("module.plugins.delivery_normal.util.DeliveryNormalUtil");
 		$conf = DeliveryNormalUtil::getDeliveryDateConfig();
 		if(isset($conf["delivery_date_mail_insert_date"]) && (int)$conf["delivery_date_mail_insert_date"] > 0){
 			$content = str_replace("#DELIVERY_DATE_AUTO#", date("Y-m-d", time() + $conf["delivery_date_mail_insert_date"] * 24 * 60 * 60), $content);
 		}
-		
+
 		//お届け時間
 		$time = (isset($list["delivery_normal.time"])) ? $list["delivery_normal.time"]["value"] : "";
 		$content = str_replace("#DELIVERY_TIME#", $time, $content);
-		
+
 		//送料
 		$list = $order->getModuleList();
 		$postage = (isset($list["delivery_normal"])) ? number_format($list["delivery_normal"]->getPrice()) : "";
 		$content = str_replace("#POSTAGE#", $postage, $content);
-		
+
 		return $content;
 	}
 }
