6712f34bdb00535508aa06c078e5c543f8340836
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-08-09 05:56:25 +0900

    CSVインポートエクスポートの初回使用時にすべてのチェックが効かない不具合を修正

--- cms/soyshop/webapp/src/logic/csv/ItemCheckListLogic.class.php
+++ cms/soyshop/webapp/src/logic/csv/ItemCheckListLogic.class.php
@@ -11,30 +11,36 @@ class ItemCheckListLogic extends SOY2LogicBase {
 	}
 
 	function buildJSCode($type = "item"){
-		$checks = SOYShop_DataSets::get($type . "_csv_item_check", null);
-		if(is_null($checks)) return "";
 
 		$scripts = array();
 		$scripts[] = "<script>";
 
-		//すべてのチェックを外す
-		$scripts[] = "$('input[name^=\"item[\"]').prop('checked', false);";
-
-		//ダミーにチェックを入れる
-		$scripts[] = "$('input[name$=\"_dummy\"]').prop('checked', true);";
-
 		//一つずつチェックを入れていく
-		foreach($checks as $key => $val){
-			if(is_string($val) && $val != 1) continue;
+		$checks = SOYShop_DataSets::get($type . "_csv_item_check", array());
+		if(count($checks)){
+			//すべてのチェックを外す
+			$scripts[] = "$('input[name^=\"item[\"]').prop('checked', false);";
 
-			//多言語プラグインの対応
-			if(is_array($val) && count($val)){
-				foreach($val as $k => $v){
-					if(is_string($v) && $v != 1) continue;
-					$scripts[] = "if($('input[name=\"item[" . $key . "][" . $k . "]\"]')) $('input[name=\"item[" . $key . "][" . $k . "]\"]').prop('checked', true);";
+			//ダミーにチェックを入れる
+			$scripts[] = "$('input[name$=\"_dummy\"]').prop('checked', true);";
+
+			foreach($checks as $key => $val){
+				if(is_string($val) && $val != 1) continue;
+
+				//多言語プラグインの対応
+				if(is_array($val) && count($val)){
+					foreach($val as $k => $v){
+						if(is_string($v) && $v != 1) continue;
+						$scripts[] = "if($('input[name=\"item[" . $key . "][" . $k . "]\"]')) $('input[name=\"item[" . $key . "][" . $k . "]\"]').prop('checked', true);";
+
+						//カスタムサーチフィールドがあった場合はもう一つチェックを追加。インポート用
+						if(strpos($key, "custom_search_field") !== false){
+							$scripts[] = "if($('input[name=\"item[" . $key . "(" . $k . ")]\"]')) $('input[name=\"item[" . $key . "(" . $k . ")]\"]').prop('checked', true);";
+						}
+					}
+				}else{
+					$scripts[] = "if($('input[name=\"item[" . $key . "]\"]')) $('input[name=\"item[" . $key . "]\"]').prop('checked', true);";
 				}
-			}else{
-				$scripts[] = "if($('input[name=\"item[" . $key . "]\"]')) $('input[name=\"item[" . $key . "]\"]').prop('checked', true);";
 			}
 		}
 
