e8496502554df7412faa9a4d76fef0458753b90e
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-10-22 05:49:07 +0900

    ラベル一覧のアコーディオンで親ラベル名にスペースがあっても動作するように修正

--- cms/soycms/webapp/pages/_component/Label/LabelsListComponent.class.php
+++ cms/soycms/webapp/pages/_component/Label/LabelsListComponent.class.php
@@ -2,15 +2,25 @@
 
 class LabelsListComponent extends HTMLList{
 
-	function populateItem($entity, $key){
+	function populateItem($entity, $key, $cnt){
 		$this->addLabel("category_name", array(
 			"text" => $key,
 			"visible" => !is_int($key) && strlen($key),
 		));
+
+		$this->addLabel("parent_category_count", array(
+			"text" => $cnt,
+		));
+
 		$this->createAdd("list", "LabelListComponent", array(
 			"list" => $entity
 		));
 
 		return ( count($entity) > 0 );
+	}
+
+	private function convert($str){
+		$str = str_replace("　", " ", $str);
+		return str_replace(" ", "_", $str);
 	}
 }
