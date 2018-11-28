1a99449797f5e00cbba78af755074e983b4f7f15
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-11-29 05:20:09 +0900

    ブロック一括変更プラグインのPHP7対応

--- cms/common/site_include/plugin/admin_block_manager/admin_block_manager.php
+++ cms/common/site_include/plugin/admin_block_manager/admin_block_manager.php
@@ -10,11 +10,11 @@ if(is_null($obj)){
 CMSPlugin::addPlugin(ADMIN_BLOCK_MANAGER,array($obj,"init"));
 
 class AdminBlockManagerPlugin{
-	
+
 	function getId(){
 		return ADMIN_BLOCK_MANAGER;
 	}
-	
+
 	function init(){
 		CMSPlugin::addPluginMenu($this->getId(),array(
 			"name"=>"ブロック一括管理プラグイン",
@@ -22,23 +22,19 @@ class AdminBlockManagerPlugin{
 			"author"=>"株式会社Brassica",
 			"url"=>"https://brassica.jp/",
 			"mail"=>"soycms@soycms.net",
-			"version"=>"1.0"
-		));	
+			"version"=>"1.1"
+		));
 		CMSPlugin::addPluginConfigPage($this->getId(),array(
 			$this,"config_page"
 		));
 	}
-	
+
 	function config_page(){
 		if(CMSPlugin::activeCheck($this->getId())){
 			include_once(dirname(__FILE__)."/index.php");
 			return get_config_page();
 		}
-		
-		return "";		
+
+		return "";
 	}
-	
 }
-
-
-?>
--- cms/common/site_include/plugin/admin_block_manager/files/ABM_StartPage.class.php
+++ cms/common/site_include/plugin/admin_block_manager/files/ABM_StartPage.class.php
@@ -1,61 +1,61 @@
 <?php
 
 class ABM_StartPage extends ABM_PageBase{
-	
+
 	var $message = array();
-	
+
 	function doPost(){
-		
+
 		if(isset($_POST["next"])){
-			
+
 			$pageList = @$_POST["page_list"];
 			if(empty($pageList)){
 				$this->message[] = "ページが選択されていません。";
 			}
-			
+
 			$blockId = @$_POST["block_id"];
 			if(strlen($blockId)<1 || !preg_match('/^[a-zA-Z0-9_-]+$/',$blockId)){
 				$this->message[] = "ブロックIDが空または不正な値です。";
 			}
-			
+
 			$class = @$_POST["class"];
-			
+
 			//値の保存
 			$session = $this->getSession();
 			$session["page_list"] = $pageList;
 			$session["block_id"] = $blockId;
 			$session["class"] = $class;
-			
+
 			$this->saveSession($session);
-			
+
 			if(empty($this->message)){
 				$this->goNext();
 			}
 		}
-		
+
 	}
-	
+
     function __construct(){
     	parent::__construct();
-    	
+
     	$session = $this->getSession();
 		$pageList = (isset($session["page_list"])) ? $session["page_list"] : array();
 		$blockId = @$session["block_id"];
 		$class = @$session["class"];
-		
+
 		if(isset($session["message"])){
 			$this->message[] = $session["message"];
 			unset($session["message"]);
 			$this->saveSession($session);
-		}		
-		
+		}
+
 		//メッセージを表示
     	$this->createAdd("message","HTMLLabel",array(
     		"html" => implode("<br>",$this->message),
-    		"visible" => (count($this->message) > 0)   	
+    		"visible" => (count($this->message) > 0)
     	));
-			
-    	
+
+
     	//ページを取得
     	$pageDAO = SOY2DAOFactory::create("cms.PageDAO");
     	$pages = $pageDAO->get();
@@ -63,7 +63,7 @@ class ABM_StartPage extends ABM_PageBase{
     		"list"=>$pages,
     		"checkedPageList" => $pageList
     	));
-    	
+
 		$this->createAdd("checkall","HTMLCheckBox",array(
 			"name"=>"checkall",
 			"type"=>"checkbox",
@@ -72,35 +72,35 @@ class ABM_StartPage extends ABM_PageBase{
 			"label"=>"チェックを反転する",
 			"selected" => false
 		));
-    	
+
     	//ブロックの種類を出力
     	$blockList = Block::getBlockComponentList();
-    	
+
     	$this->createAdd("component_loop","BlockList",array(
     		"list"=>$blockList,
     		"selectedClassName" => $class
     	));
-    	
+
     	//ブロックIDを出力
     	$this->createAdd("block_id","HTMLInput",array(
     		"name" => "block_id",
     		"value" => $blockId
     	));
     }
-    
+
     function getTemplateFilePath(){
 		return dirname(__FILE__) . "/ABM_StartPage.html";
 	}
 }
 
 class BlockList extends HTMLList{
-	
+
 	var $selectedClassName = "EntryBlockComponent";
-	
+
 	function setSelectedClassName($className){
 		$this->selectedClassName = $className;
 	}
-	
+
 	function populateItem($entity){
 
 		$this->createAdd("component_check","HTMLCheckBox",array(
@@ -111,19 +111,19 @@ class BlockList extends HTMLList{
 			"label"=>$entity->getComponentName(),
 			"selected" => ($this->selectedClassName == get_class($entity))
 		));
-	}	
+	}
 }
 
 class PageList extends HTMLList{
-	
+
 	var $checkedPageList = array();
-	
+
 	function setCheckedPageList($list){
 		$this->checkedPageList = $list;
 	}
-	
+
 	function populateItem($entity){
-		
+
 		$this->createAdd("checkbox","HTMLCheckbox",array(
 			"name" => "page_list[]",
 			"type" => "checkbox",
@@ -131,15 +131,14 @@ class PageList extends HTMLList{
 			"selected" => (in_array($entity->getId(),$this->checkedPageList)),
 			"label" => $entity->getUri()
 		));
-		
+
 		$this->createAdd("name","HTMLLabel",array(
 			"text" => $entity->getTitle()
 		));
-		
+
 //		$this->createAdd("url","HTMLLabel",array(
 //			"text" => $entity->getUri()
 //		));
-		
+
 	}
 }
-?>
