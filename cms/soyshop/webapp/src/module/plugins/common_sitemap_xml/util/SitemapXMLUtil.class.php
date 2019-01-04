886cfbca94ec0605be5ff471b55be3b937fe428c
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-01-04 17:03:42 +0900

    リファクタリング

--- cms/soyshop/webapp/src/module/plugins/common_sitemap_xml/util/SitemapXMLUtil.class.php
+++ cms/soyshop/webapp/src/module/plugins/common_sitemap_xml/util/SitemapXMLUtil.class.php
@@ -2,11 +2,11 @@
 
 class SitemapXMLUtil{
 
-  public static function getConfig(){
-    return SOYShop_DataSets::get("sitemap_xml.config", array());
-  }
+	public static function getConfig(){
+		return SOYShop_DataSets::get("sitemap_xml.config", array());
+	}
 
-  public static function saveConfig($values){
-    SOYShop_DataSets::put("sitemap_xml.config", $values);
-  }
+	public static function saveConfig($values){
+    	SOYShop_DataSets::put("sitemap_xml.config", $values);
+	}
 }
