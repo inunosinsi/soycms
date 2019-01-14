f5ded20f70bda1b85165aff89640daae6ae745d2
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-01-15 04:58:14 +0900

    操作者の履歴の保持の仕方に誤りがあったため修正

--- cms/soyshop/webapp/src/domain/order/SOYShop_OrderStateHistoryDAO.class.php
+++ cms/soyshop/webapp/src/domain/order/SOYShop_OrderStateHistoryDAO.class.php
@@ -27,9 +27,26 @@ abstract class SOYShop_OrderStateHistoryDAO extends SOY2DAO{
      */
     function onInsert($query, $binds){
     	if(!isset($binds[":date"])) $binds[":date"] = time();
-    	if(!isset($binds[":author"])){
-			SOY2::import("domain.config.SOYShop_ShopConfig");
-			$binds[":author"] = SOYShop_ShopConfig::load()->getAutoOperateAuthorId();
+    	if(!isset($binds[":author"]) || !strlen($binds[":author"])){
+			/*
+    		 * 管理画面では管理者情報をを登録する
+    		 * SOY ShopでUserInfoUtilが使えることはないのでは？
+    		 */
+    		if(class_exists("UserInfoUtil")){
+    			$author = UserInfoUtil::getUserName();
+				if(!strlen($author)) $author = UserInfoUtil::getUserId();
+    		}else{
+    			$author = SOY2ActionSession::getUserSession()->getAttribute("username");
+				if(!strlen($author)) $author = SOY2ActionSession::getUserSession()->getAttribute("userid");
+    		}
+
+			//カートからの注文の場合は設定画面で決めたauthorにする
+			if(!strlen($author)){
+				SOY2::import("domain.config.SOYShop_ShopConfig");
+				$author = SOYShop_ShopConfig::load()->getAutoOperateAuthorId();
+			}
+
+			$binds[":author"] = $author;
     	}
     	return array($query, $binds);
     }
