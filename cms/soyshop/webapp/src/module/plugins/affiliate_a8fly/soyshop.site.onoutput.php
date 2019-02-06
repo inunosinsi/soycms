e9acb51ec4a0f10fbb9d1115eefcd82569e8939e
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-02-06 10:00:59 +0900

    A8.netプラグインでITFに対応

---
+++ cms/soyshop/webapp/src/module/plugins/affiliate_a8fly/soyshop.site.onoutput.php
@@ -0,0 +1,39 @@
+<?php
+
+class AffiliateA8flyOnOutput extends SOYShopSiteOnOutputAction{
+
+	/**
+	 * @return string
+	 */
+	function onOutput($html){
+		//XHTMLではないXMLでは出力しない
+		if(
+			strpos($html, '<?xml version="1"') !== false
+			&&
+			strpos($html, '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML') === false
+		){
+			return $html;
+		}
+
+		//RSS, Atomでは出力しない
+		if(
+			strpos($html, '<rss version="2.0">') !== false
+			||
+			strpos($html, '<feed xml:lang="ja" xmlns="http://www.w3.org/2005/Atom">') !== false
+		){
+			return $html;
+		}
+
+		//</head>の直前。なければ挿入しない
+		if(stripos($html, '</head>') === false) return $html;
+
+		SOY2::import("module.plugins.affiliate_a8fly.util.AffiliateA8flyUtil");
+		$config = AffiliateA8flyUtil::getConfig();
+		if(!isset($config["id"]) || !strlen($config["id"])) return $html;
+
+		$tag = "<script src=\"//statics.a8.net/a8sales/a8sales.js\"></script>";
+		return str_ireplace('</head>', $tag . "\n" . '</head>', $html);
+	}
+}
+
+SOYShopPlugin::extension("soyshop.site.onoutput", "affiliate_a8fly", "AffiliateA8flyOnOutput");
