de0bafc718f75815022262aae6d65903c8f953bf
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-04-11 11:54:56 +0900

    注文再登録プラグインで顧客情報も自動入力する設定を追加

--- cms/soyshop/webapp/src/module/plugins/order_re_register/soyshop.order.function.php
+++ cms/soyshop/webapp/src/module/plugins/order_re_register/soyshop.order.function.php
@@ -17,20 +17,49 @@ class OrderReRegisterFunction extends SOYShopOrderFunction{
 	 * @return html
 	 */
 	function getPage(){
+		include_once(SOYSHOP_WEBAPP . "pages/Order/Register/common.php");
+		$cart = AdminCartLogic::getCart();
+
 		try{
 			$itemOrders = self::itemOrderDao()->getByOrderId($this->getOrderId());
 		}catch(Exception $e){
 			$itemOrders = array();
 		}
 
+		$doSave = false;
 		if(count($itemOrders)){
-			include_once(SOYSHOP_WEBAPP . "pages/Order/Register/common.php");
-			$cart = AdminCartLogic::getCart();
 			foreach($itemOrders as $itemOrder){
-				$cart->addItem($itemOrder->getItemId(), $itemOrder->getItemCount());
+				if($itemOrder->getItemId() > 0){
+					$cart->addItem($itemOrder->getItemId(), $itemOrder->getItemCount());
+				}else{	//登録外商品の場合
+					$cart->addUnlistedItem($itemOrder->getItemName(), $itemOrder->getItemCount(), $itemOrder->getItemPrice());
+				}
+				$doSave = true;
 			}
-			$cart->save();
 		}
+
+		//顧客情報の登録
+		SOY2::import("module.plugins.order_re_register.util.ReRegiserUtil");
+		$config = ReRegiserUtil::getConfig();
+		if(isset($config["customer"]) && $config["customer"] == 1){
+			try{
+				$userId = SOY2DAOFactory::create("order.SOYShop_OrderDAO")->getById($this->getOrderId())->getUserId();
+			}catch(Exception $e){
+				$userId = null;
+			}
+
+			if(isset($userId)){
+				try{
+					$user = SOY2DAOFactory::create("user.SOYShop_UserDAO")->getById($userId);
+					$cart->setCustomerInformation($user);
+					$doSave = true;
+				}catch(Exception $e){
+					//
+				}
+			}
+		}
+
+		if($doSave) $cart->save();
 
 		SOY2PageController::jump("Order.Register");
 	}
