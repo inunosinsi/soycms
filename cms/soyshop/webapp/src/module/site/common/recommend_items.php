1e62e32e6d6b802a6feb5a7cfcd893d6dc9e7367
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-07-17 10:27:45 +0900

    SOY CMS側でおすすめ商品設定機能を使用すると500 Internal Errorになる件を修正

--- cms/soyshop/webapp/src/module/site/common/recommend_items.php
+++ cms/soyshop/webapp/src/module/site/common/recommend_items.php
@@ -8,17 +8,17 @@ function soyshop_recommend_items($html, $htmlObj){
 	$dao = SOY2DAOFactory::create("shop.SOYShop_ItemDAO");
 	$ids = SOYShop_DataSets::get("item.recommend_items", array());
 	$items = array();
-	
+
 	if(count($ids)){
-		
+
 		SOY2::import("module.plugins.common_recommend_item.util.RecommendItemUtil");
 		$config = RecommendItemUtil::getConfig();
-		
+
 		$sql = "SELECT * FROM soyshop_item ".
              "WHERE id IN (". implode(",", $ids) . ") ".
              "AND item_is_open != 0 ".
              "AND is_disabled != 1 ";
-				
+
 		//ソートの設定
 		if(isset($config["defaultSort"])){
             //ランダム表示
@@ -41,16 +41,16 @@ function soyshop_recommend_items($html, $htmlObj){
 					$key = "item_" . $config["defaultSort"];
                 }
                 $sql .= "ORDER BY " . $key . " ";
-                $sql .= ($config["isReverse"] == 1) ? "ASC" : "DESC";    
-            }	
-		}		
-		
+                $sql .= ($config["isReverse"] == 1) ? "ASC" : "DESC";
+            }
+		}
+
 		try{
 			$res = $dao->executeQuery($sql, array());
 		}catch(Exception $e){
 			$res = array();
 		}
-		
+
 		if(count($res)){
 			foreach($res as $map){
 				if(!isset($map["id"])) continue;
@@ -59,6 +59,7 @@ function soyshop_recommend_items($html, $htmlObj){
 		}
 	}
 
+	SOY2::import("base.site.classes.SOYShop_ItemListComponent");
 	$obj->createAdd("recommend_item_list", "SOYShop_ItemListComponent", array(
 		"list" => $items,
 		"soy2prefix" => SOYSHOP_SITE_PREFIX,//cms:idは互換性維持のため残しておく
@@ -77,4 +78,3 @@ function soyshop_recommend_items($html, $htmlObj){
 		ob_end_clean();
 	}
 }
-?>
