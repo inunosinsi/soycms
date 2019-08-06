f993cdd6d77a8e89ee9925c5c398bce6e1b03f1d
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-08-06 16:06:37 +0900

    ファイルアップロードカラムでアップロード直後に画像ファイルをリサイズする

--- cms/app/webapp/inquiry/src/columns/FileColumn.class.php
+++ cms/app/webapp/inquiry/src/columns/FileColumn.class.php
@@ -4,6 +4,10 @@ class FileColumn extends SOYInquiry_ColumnBase{
 
 	private $extensions = "jpg,jpeg,gif,png";
 	private $uploadsize = 500;	//KB
+	private $resize_w;
+	private $resize_h;
+
+	/** @ToDo リサイズで縦長過ぎる画像をアップロードされたらどうする？ **/
 
 	const KB_SIZE = 1024;
 
@@ -42,8 +46,10 @@ class FileColumn extends SOYInquiry_ColumnBase{
 	 * 設定画面で表示する用のフォーム
 	 */
 	function getConfigForm(){
-		$html  = 'サイズ:<input type="text" name="Column[config][uploadsize]" value="'.htmlspecialchars($this->uploadsize,ENT_QUOTES).'" size="3"/>KB&nbsp;';
-		$html .= '拡張子:<input type="text" name="Column[config][extensions]" value="'.htmlspecialchars($this->extensions,ENT_QUOTES).'" /><br>';
+		$html  = 'サイズ:<input type="text" name="Column[config][uploadsize]" value="'.htmlspecialchars($this->uploadsize,ENT_QUOTES).'" size="3">KB&nbsp;';
+		$html .= '拡張子:<input type="text" name="Column[config][extensions]" value="'.htmlspecialchars($this->extensions,ENT_QUOTES).'"><br>';
+		$html .= '画像のリサイズ: width:<input type="text" name="Column[config][resize_w]" value="'.htmlspecialchars($this->resize_w, ENT_QUOTES) . '" size="5">px ';
+		$html .= 'height:<input type="text" name="Column[config][resize_h]" value="'.htmlspecialchars($this->resize_h, ENT_QUOTES) . '" size="5">px(アスペクト比は維持)';
 
 		return $html;
 	}
@@ -55,8 +61,18 @@ class FileColumn extends SOYInquiry_ColumnBase{
 		$value = $this->getValue();
 
 		if(is_array($value) && isset($value["name"]) && isset($value["size"])){
-			$html = $value["name"] . " (".(int)($value["size"] / self::KB_SIZE)."KB)";
-			return htmlspecialchars($html, ENT_QUOTES, "UTF-8");
+/**
+			$imgPath = str_replace(SOY_INQUIRY_UPLOAD_DIR, "", $value["tmp_name"]);
+			$siteId = trim(substr(_SITE_ROOT_, strrpos(_SITE_ROOT_, "/")), "/");
+
+			//リサイズ
+			$resizeW = (is_numeric($this->resize_w) && $this->resize_w > 150) ? 150 : $this->resize_w;
+
+			$html = "<img src=\"/" . $siteId . "/im.php?src=/" . $imgPath . "&width=" . $resizeW . "\"><br>";
+			**/
+			$html = "";
+			$html .= htmlspecialchars($value["name"] . " (".(int)($value["size"] / self::KB_SIZE)."KB)", ENT_QUOTES, "UTF-8");
+			return $html;
 		}
 
 		return "";
@@ -188,6 +204,22 @@ class FileColumn extends SOYInquiry_ColumnBase{
 			return false;
 		}
 
+		//$path_toにあるファイルをリサイズする @ToDo 画像ファイルであるか？を調べた後に実行
+		if(strpos($type, "image") !== false && is_numeric($this->resize_w) && $this->resize_w > 0){
+			$imgInfo = getimagesize($path_to);
+			if(is_array($imgInfo) && count($imgInfo) >= 7){	//idx:0がwidthでidx:1がheight
+				if($imgInfo[0] >= $imgInfo[1]){	//横長画像もしくは正方形
+					$resizeW = $this->resize_w;
+					$resizeH = (int)($this->resize_w * $imgInfo[1] / $imgInfo[0]);
+				}else{ //縦長画像
+					$resizeW = (int)($this->resize_h * $imgInfo[0] / $imgInfo[1]);
+					$resizeH = $this->resize_h;
+				}
+				soy2_resizeimage($path_to, $path_to, $resizeW, $resizeH);
+				$size = filesize($path_to);
+			}
+		}
+
 
 		//もしアップロードされているのであれば値を保存する
 		$this->setValue(array(
@@ -200,8 +232,6 @@ class FileColumn extends SOYInquiry_ColumnBase{
 
 		$new_value = base64_encode(serialize($this->getValue()));
 		$_POST["data"][$this->getColumnId()] = $new_value;
-
-
 	}
 
 	/**
@@ -212,12 +242,20 @@ class FileColumn extends SOYInquiry_ColumnBase{
 
 		$this->uploadsize  = (isset($config["uploadsize"]) && is_numeric($config["uploadsize"])) ? (int)$config["uploadsize"] : 500;
 		$this->extensions = (isset($config["extensions"]) && strlen($config["extensions"])) ? $config["extensions"] : "jpg,gif,png";
+		$this->resize_w = (isset($config["resize_w"]) && is_numeric($config["resize_w"])) ? (int)$config["resize_w"] : null;
+		$this->resize_h = (isset($config["resize_h"]) && is_numeric($config["resize_h"])) ? (int)$config["resize_h"] : null;
 	}
 
 	function getConfigure(){
 		$config = parent::getConfigure();
 		$config["uploadsize"] = $this->uploadsize;
 		$config["extensions"] = $this->extensions;
+		$config["resize_w"] = (isset($this->resize_w) && is_numeric($this->resize_w)) ? (int)$this->resize_w : null;
+		$config["resize_h"] = (isset($this->resize_h) && is_numeric($this->resize_h)) ? (int)$this->resize_h : null;
+
+		//片方しか値を登録していない場合はwidthとheightで補完し合う。
+		if(is_null($config["resize_w"]) && is_numeric($config["resize_h"])) $config["resize_w"] = $config["resize_h"];
+		if(is_null($config["resize_h"]) && is_numeric($config["resize_w"])) $config["resize_h"] = $config["resize_w"];
 		return $config;
 	}
 
