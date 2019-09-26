a43209d5bef0256496821f889092a44600836d35
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2019-09-27 06:49:36 +0900

    ログアウト時にauto loginのトークンを削除できなかった件を修正

--- cms/common/action/login/LogoutAction.class.php
+++ cms/common/action/login/LogoutAction.class.php
@@ -3,20 +3,19 @@
 class LogoutAction extends SOY2Action{
 
     function execute() {
-    	UserInfoUtil::logout();
-
 		//autoログインも削除
 		if(isset($_COOKIE["soycms_auto_login"])){
 			$dao = SOY2DAOFactory::create("admin.AutoLoginDAO");
 			try{
 				$login = $dao->getByToken($_COOKIE["soycms_auto_login"]);
-				$login->setLimit(time() - 1);
 				setcookie("soycms_auto_login", $login->getToken(), time() - 1);
-				$dao->update($login);
+				$dao->deleteByUserId($login->getUserId());
 			}catch(Exception $e){
 				//
 			}
 		}
+
+		UserInfoUtil::logout();
 
     	return SOY2Action::SUCCESS;
     }
