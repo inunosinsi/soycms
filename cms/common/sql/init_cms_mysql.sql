a2f20e64d2c746228b80bf6b2410c2966cfcaadc
inunosinsi <tsuyoshi.s.saito@gmail.com>
2019-10-29 13:01:07 +0900

    SQL構文にタイポ

--- cms/common/sql/init_cms_mysql.sql
+++ cms/common/sql/init_cms_mysql.sql
@@ -22,7 +22,7 @@ CREATE TABLE Administrator (
 
 CREATE TABLE AdministratorAttribute (
 	admin_id INTEGER NOT NULL,
-	admin_field_id VARCHAR(255) NOT NULLs,
+	admin_field_id VARCHAR(255) NOT NULL,
 	admin_value TEXT,
 	unique(admin_id, admin_field_id)
 )ENGINE = InnoDB;
