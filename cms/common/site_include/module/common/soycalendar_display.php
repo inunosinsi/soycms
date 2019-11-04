c6d5a8f5bf02c10ca47f694bf43917cef190133c
tsuyoshi saito <tsuyoshi@saitodev.co>
2019-11-05 05:05:11 +0900

    cms:module版のSOY Calendarでspecify calendarを追加

--- cms/common/site_include/module/common/soycalendar_display.php
+++ cms/common/site_include/module/common/soycalendar_display.php
@@ -26,6 +26,22 @@ function soycms_soycalendar_display($html, $page){
 		"html" => $logic->getNextCalendar(false, true)
 	));
 
+	//任意の指定月のカレンダー
+	for($i = 1; $i <= 10; $i++){
+		$cal = "";
+		if(strpos($html, "cms:id=\"specify_calender_" . $i . "\"")){
+			preg_match('/cms:id="specify_calender_' . $i . '?".*?cms:specify="(.*?)"/', $html, $tmp);
+			if(isset($tmp[1]) && is_numeric($tmp[1])){
+				$cal = $logic->getSpecifyCalendar(false, true, (int)$tmp[1]);
+			}
+		}
+
+		$obj->addLabel("specify_calender_" . $i, array(
+			"soy2prefix" => $prefix,
+			"html" => $cal
+		));
+	}
+
 	SOYAppUtil::resetAppMode($old);
 
 	$obj->display();
