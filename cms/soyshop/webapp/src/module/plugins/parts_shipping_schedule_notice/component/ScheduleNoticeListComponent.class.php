46d564fa06b489449d8f20692f9dd459d1d719af
tsuyoshi saito <tsuyoshi@saitodev.co>
2019-11-21 06:23:06 +0900

    出荷予定日通知プラグイン

---
+++ cms/soyshop/webapp/src/module/plugins/parts_shipping_schedule_notice/component/ScheduleNoticeListComponent.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class ScheduleNoticeListComponent extends HTMLList {
+
+	private $config;
+
+	protected function populateItem($entity){
+		$this->addLabel("label", array(
+			"text" => (isset($entity)) ? ShippingScheduleUtil::getLabel($entity) : ""
+		));
+
+		$this->addInput("schedule", array(
+			"name" => "Config[schedule][" . $entity . "]",
+			"value" => (isset($this->config["schedule"][$entity])) ? (int)$this->config["schedule"][$entity] : 1,
+			"style" => "width:40px;"
+		));
+
+		$this->addTextArea("wording", array(
+			"name" => "Config[notice][" . $entity . "]",
+			"value" => (isset($this->config["notice"][$entity])) ? $this->config["notice"][$entity] : ""
+		));
+	}
+
+	function setConfig($config){
+		$this->config = $config;
+	}
+}
