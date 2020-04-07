780e82358f73444bfb0ced6a426f032a9efaeb24
tsuyoshi saito <tsuyoshi@saitodev.co>
2020-04-07 16:14:18 +0900

    magic_quoteの廃止

--- cms/common/lib/magic_quote_gpc.php
+++ cms/common/lib/magic_quote_gpc.php
@@ -1,18 +1,19 @@
 <?php
-//magic_quotes_gpc対策
-if(get_magic_quotes_gpc()){
-
+//ドットを外し、前2桁取得
+$phpVersion = (int)substr(str_replace(".", "", phpversion()), 0, 2);
+//magic_quotes_gpc対策 PHP7.4.Xでは推奨されていないらしい
+if($phpVersion < 74 && get_magic_quotes_gpc()){
+	
 	function _stripslashes($value){
 		$value = is_array($value) ?
 					array_map('_stripslashes', $value) :
 					stripslashes($value);
     	return $value;
 	}
-	
+
 	$_POST = _stripslashes($_POST);
 	$_GET = _stripslashes($_GET);
 	$_COOKIE = _stripslashes($_COOKIE);
 	$_REQUEST = _stripslashes($_REQUEST);
 
 }
-?>
