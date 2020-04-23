87336d3d254de1e325ca862daf4de6cd3bc9d188
tsuyoshi saito <tsuyoshi@saitodev.co>
2020-04-23 14:35:13 +0900

    PHP7以降でmagic_quoteが禁止になっていた

--- cms/soyshop/webapp/lib/magic_quote_gpc.php
+++ cms/soyshop/webapp/lib/magic_quote_gpc.php
@@ -1,6 +1,8 @@
 <?php
-//magic_quotes_gpc対策
-if(get_magic_quotes_gpc()){
+//ドットを外し、前2桁取得
+$phpVersion = (int)substr(str_replace(".", "", phpversion()), 0, 2);
+//magic_quotes_gpc対策 PHP7.4.Xでは推奨されていないらしい
+if($phpVersion < 74 && get_magic_quotes_gpc()){
 
 	if(!function_exists("_stripslashes")){
 		function _stripslashes($value){
