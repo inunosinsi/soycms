f9b4ac0dc5880dbb9d47e3c277ab0239930d3915
inunosinsi <tsuyoshi@saitodev.co>
2020-04-30 15:42:57 +0900

    リファクタリング

--- cms/soycms/webapp/config.inc.php
+++ cms/soycms/webapp/config.inc.php
@@ -102,8 +102,4 @@ if(!UserInfoUtil::isLoggined()){
 }
 
 //update event
-SOY2DAOConfig::setUpdateQueryEvent(function($sql,$binds) {
-	//sqlでAutoLoginがある場合は処理を止める
-	if(is_numeric(strpos($sql, "AutoLogin"))) return;
-	touch(UserInfoUtil::getSiteDirectory().".db/".SOYCMS_DB_TYPE.".db");
-});
+SOY2DAOConfig::setUpdateQueryEvent(function($sql,$binds) { touch(UserInfoUtil::getSiteDirectory().".db/".SOYCMS_DB_TYPE.".db"); });
