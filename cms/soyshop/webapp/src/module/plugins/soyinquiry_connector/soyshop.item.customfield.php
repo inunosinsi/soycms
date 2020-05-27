1a53c1497b43097dcae2574cf2be64bdfc750feb
inunosinsi <tsuyoshi@saitodev.co>
2020-05-28 06:53:38 +0900

    β15

--- cms/soyshop/webapp/src/module/plugins/soyinquiry_connector/soyshop.item.customfield.php
+++ cms/soyshop/webapp/src/module/plugins/soyinquiry_connector/soyshop.item.customfield.php
@@ -9,24 +9,24 @@ class SOYInquiryConnectorCustomField extends SOYShopItemCustomFieldBase{
 	function onOutput($htmlObj, SOYShop_Item $item){
 		include_once(dirname(__FILE__) . "/common.php");
 		$config = SOYInquiryConnectorCommon::getConfig();
-		
+
 		$url = "";
-		
+
 		if(isset($config["url"])){
 			$url = $config["url"];
 		}
-		
+
 		//すでにGETの値がある場合
 		if(strpos($url, "?")!==false){
-			
+
 			$url .= "&amp;item_id=" . $item->getId();
-			
+
 		//ない場合
 		}else{
-			
+
 			$url .= "?item_id=" . $item->getId();
 		}
-		
+
 		$htmlObj->addLink("inquiry_link", array(
 			"soy2prefix" => SOYSHOP_SITE_PREFIX,
 			"link" => $url
@@ -35,4 +35,3 @@ class SOYInquiryConnectorCustomField extends SOYShopItemCustomFieldBase{
 }
 
 SOYShopPlugin::extension("soyshop.item.customfield","soyinquiry_connector","SOYInquiryConnectorCustomField");
-?>
