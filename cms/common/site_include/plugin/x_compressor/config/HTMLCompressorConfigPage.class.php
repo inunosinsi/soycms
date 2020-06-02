e5f9a7e488e126594b47788ef0a878b538e8dc2e
inunosinsi <tsuyoshi@saitodev.co>
2020-06-02 13:53:26 +0900

    HTML圧縮プラグイン

---
+++ cms/common/site_include/plugin/x_compressor/config/HTMLCompressorConfigPage.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class HTMLCompressorConfigPage extends WebPage {
+
+	private $pluginObj;
+
+	function __construct(){
+		SOY2::import("site_include.CMSPage");
+		SOY2::import("site_include.CMSBlogPage");
+		SOY2::import("site_include.plugin.x_html_cache.component.PageListComponent");
+	}
+
+	function doPost(){
+		if(soy2_check_token()){
+			$this->pluginObj->config_per_page = (isset($_POST["config_per_page"])) ? $_POST["config_per_page"] : array();
+			$this->pluginObj->config_per_blog = (isset($_POST["config_per_blog"])) ? $_POST["config_per_blog"] : array();
+
+			CMSUtil::notifyUpdate();
+			CMSPlugin::savePluginConfig($this->pluginObj->getId(), $this->pluginObj);
+			CMSPlugin::redirectConfigPage();
+		}
+	}
+
+	function execute(){
+		parent::__construct();
+
+		$this->addLabel("page_controller_path", array(
+			"text" => UserInfoUtil::getSiteDirectory() . "index.php"
+		));
+
+		$this->addForm("form");
+
+		//挿入するページの指定
+		$this->createAdd("page_list","PageListComponent",array(
+			"list"  => self::getPages(),
+			"pluginObj" => $this->pluginObj
+		));
+	}
+
+	function getPluginObj() {
+		return $this->pluginObj;
+	}
+	function setPluginObj($pluginObj) {
+		$this->pluginObj = $pluginObj;
+	}
+
+	private function getPages(){
+    	$result = SOY2ActionFactory::createInstance("Page.PageListAction",array(
+    		"offset" => 0,
+    		"count"  => 1000,
+    		"order"  => "cdate"
+    	))->run();
+
+    	return $result->getAttribute("PageList");// + $result->getAttribute("RemovedPageList");
+	}
+}
---
+++ cms/common/site_include/plugin/x_compressor/x_compressor.php
@@ -0,0 +1,141 @@
+<?php
+CompressorPlugin::register();
+
+class CompressorPlugin{
+
+	const PLUGIN_ID = "x_compressor";
+
+	//挿入するページ
+	var $config_per_page = array();
+	var $config_per_blog = array();
+
+	function getId(){
+		return self::PLUGIN_ID;
+	}
+
+	function init(){
+		CMSPlugin::addPluginMenu(self::PLUGIN_ID,array(
+			"name"=>"HTML圧縮プラグイン",
+			"description"=>"HTMLを圧縮して、サーバ間のデータ転送を高速化する",
+			"author"=>"齋藤毅",
+			"url"=>"https://saitodev.co/",
+			"mail"=>"tsuyoshi@saitodev.co",
+			"version"=>"0.1"
+		));
+		CMSPlugin::addPluginConfigPage(self::PLUGIN_ID,array(
+			$this,"config_page"
+		));
+
+		if(CMSPlugin::activeCheck(self::PLUGIN_ID)){
+			//管理画面側
+			if(!defined("_SITE_ROOT_")){
+				//何もしない
+			//公開側
+			}else{
+				CMSPlugin::setEvent('onOutput',self::PLUGIN_ID, array($this,"onOutput"), array("filter"=>"all"));
+			}
+		}
+	}
+
+	function onOutput($arg){
+		$html = &$arg["html"];
+		$page = &$arg["page"];
+
+		//アプリケーションページと404ページの場合は静的化しない→静的化プラグイン等と併用しても意味がないため
+		if($page->getPageType() == Page::PAGE_TYPE_APPLICATION || $page->getPageType() == Page::PAGE_TYPE_ERROR) return $html;
+
+		//HTML圧縮の対象ページであるか？
+		if(!isset($this->config_per_page[$page->getId()]) || $this->config_per_page[$page->getId()] != 1) return $html;
+
+		switch($page->getPageType()){
+			case Page::PAGE_TYPE_BLOG:
+				$webPage = &$arg["webPage"];
+				switch($webPage->mode){
+					case CMSBlogPage::MODE_TOP:
+					case CMSBlogPage::MODE_ENTRY:
+					case CMSBlogPage::MODE_MONTH_ARCHIVE:
+					case CMSBlogPage::MODE_CATEGORY_ARCHIVE:
+						if(!isset($this->config_per_blog[$page->getId()][$webPage->mode]) || $this->config_per_blog[$page->getId()][$webPage->mode] != 1) return $html;
+						break;
+					case CMSBlogPage::MODE_RSS:
+					case CMSBlogPage::MODE_POPUP:
+						return $html;
+				}
+				break;
+			case Page::PAGE_TYPE_NORMAL:
+			case Page::PAGE_TYPE_APPLICATION:
+			default:
+				//何もしない
+		}
+
+		//圧縮を行う
+		$lines = explode("\n", $html);
+		$lineCnt = count($lines);
+		if($lineCnt === 0) return $html;
+
+		$isHTML5 = false;
+
+		$htmls = array();
+		for($i = 0; $i < $lineCnt; $i++){
+			$line = trim($lines[$i]);
+			if(!strlen($line)) continue;
+			$line = str_replace("\t", "", $line);
+			if(!strlen($line)) continue;
+
+			if(!$isHTML5 && strpos($line, "!DOCTYPE html") !== false) $isHTML5 = true;
+
+			//半角スペースが２つ続く場合は一つのする
+			$line = self::_deleteSpace($line);
+
+			//imgのaltの値が空である場合は削除
+			if(strpos($line, "alt=\"\"") !== false){
+				$line = str_replace("alt=\"\"", "", $line);
+			}
+
+			//xhtmlの記述をなくす
+			if($isHTML5) $line = self::_xhtml2html5($line);
+
+			//半角スペースが２つ続く場合は一つのする
+			$line = self::_deleteSpace($line);
+			if(!strlen($line)) continue;
+
+			$h[] = $line;
+		}
+
+		return implode("", $h);
+	}
+
+	//半角スペースが２つ続いている場合は１つのする
+	private function _deleteSpace($line){
+		for(;;){
+			if(strpos($line, "  ") === false) break;
+			$line = str_replace("  ", " ", $line);
+		}
+		return $line;
+	}
+
+	private function _xhtml2html5($line){
+		for(;;){
+			if(strpos($line, " />") === false) break;
+			$line = str_replace(" />", ">", $line);
+			if(strpos($line, " >") !== false){
+				$line = str_replace(" >", ">", $line);
+			}
+		}
+		return $line;
+	}
+
+	function config_page(){
+		SOY2::import("site_include.plugin.x_compressor.config.HTMLCompressorConfigPage");
+		$form = SOY2HTMLFactory::createInstance("HTMLCompressorConfigPage");
+		$form->setPluginObj($this);
+		$form->execute();
+		return $form->getObject();
+	}
+
+	public static function register(){
+		$obj = CMSPlugin::loadPluginConfig(self::PLUGIN_ID);
+		if(!$obj) $obj = new CompressorPlugin();
+		CMSPlugin::addPlugin(self::PLUGIN_ID, array($obj, "init"));
+	}
+}
