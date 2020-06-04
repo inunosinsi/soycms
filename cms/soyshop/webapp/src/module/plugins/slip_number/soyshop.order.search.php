d292725e8d9ca9168a9f4a417daa3e2d72a790e9
inunosinsi <tsuyoshi@saitodev.co>
2020-06-05 06:43:24 +0900

    伝票番号プラグインで伝票番号未登録の注文の検索項目を追加

--- cms/soyshop/webapp/src/module/plugins/slip_number/soyshop.order.search.php
+++ cms/soyshop/webapp/src/module/plugins/slip_number/soyshop.order.search.php
@@ -3,36 +3,54 @@
 class SlipNumberSearch extends SOYShopOrderSearch{
 
 	function setParameter($params){
-		$params = self::getParameters($params);
-		if(count($params)){
-			$binds = array();
+		$queries = array();
+		$binds = array();
+
+		//伝票番号が登録されているものを検索
+		$numbers = (isset($params["numbers"]) && strlen($params["numbers"])) ? self::_getParameters($params["numbers"]) : array();
+		if(count($numbers)){
 			$q = array();
 			$i = 0;
-			foreach($params as $param){
-				$param = trim($param);
+			foreach($numbers as $n){
+				$n = trim($n);
+				if(!strlen($n)) continue;
 				$q[] = "slip_number LIKE :slip" . $i;
-				$binds[":slip" . $i++] = "%" . $param . "%";
+				$binds[":slip" . $i++] = "%" . $n . "%";
 			}
 
 			if(count($q)){
-				$queries = array();
 				$queries[] = "id IN (SELECT order_id FROM soyshop_slip_number WHERE " . implode(" OR ", $q) . ")";
-				return array("queries" => $queries, "binds" => $binds);
 			}
 		}
+
+		//伝票番号が未登録の注文を検索する
+		if(isset($params["none"]) && $params["none"] == 1){
+			$queries[] = "id NOT IN (SELECT order_id FROM soyshop_slip_number)";
+		}
+
+		return array("queries" => $queries, "binds" => $binds);
 	}
 
 	function searchItems($params){
-		$html = "<input type=\"text\" name=\"search[customs][slip_number]\" value=\"" . self::getParameter($params) . "\" placeholder=\"スペース区切りで複数ワードで検索できます。\" style=\"width:95%;\">";
-		return array("label" => "発送伝票番号", "form" => $html);
+		$ipt = (isset($params["numbers"]) && strlen($params["numbers"])) ? self::_getParameter($params["numbers"]) : "";
+
+		$html = array();
+		$html[] = "<input type=\"text\" name=\"search[customs][slip_number][numbers]\" value=\"" . $ipt . "\" placeholder=\"スペース区切りで複数ワードで検索できます。\" style=\"width:95%;\">";
+
+		if(isset($params["none"]) && $params["none"] == 1){
+			$html[] = "<label><input type=\"checkbox\" name=\"search[customs][slip_number][none]\" value=\"1\" checked=\"checked\"> 伝票番号未登録の注文</label>";
+		}else{
+			$html[] = "<label><input type=\"checkbox\" name=\"search[customs][slip_number][none]\" value=\"1\"> 伝票番号未登録の注文</label>";
+		}
+		return array("label" => "発送伝票番号", "form" => implode("<br>", $html));
 	}
 
-	private function getParameter($param){
+	private function _getParameter($param){
 		return (!is_array($param) && is_string($param) && strlen($param)) ? $param : null;
 	}
 
-	private function getParameters($param){
-		$str = str_replace("　", " ", self::getParameter($param));
+	private function _getParameters($param){
+		$str = str_replace("　", " ", self::_getParameter($param));
 		return (strlen($str)) ? explode(" ", $str) : array();
 	}
 }
