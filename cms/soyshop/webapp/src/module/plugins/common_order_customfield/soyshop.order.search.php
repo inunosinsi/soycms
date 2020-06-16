3526de9846498e6a85efa1be623d8d54f6f70780
inunosinsi <tsuyoshi@saitodev.co>
2020-06-16 12:26:20 +0900

    オーダーカスタムフィールドでセレクトボックスのみ注文検索の項目を出力

---
+++ cms/soyshop/webapp/src/module/plugins/common_order_customfield/soyshop.order.search.php
@@ -0,0 +1,90 @@
+<?php
+
+SOY2::import("module.plugins.common_order_date_customfield.component.CustomfieldDateSearchFormComponent");
+class OrderCustomfieldOrderSearch extends SOYShopOrderSearch{
+
+	private $dao;
+	private $list;
+
+	private function prepare(){
+		if(!$this->dao){
+			$this->dao = SOY2DAOFactory::create("order.SOYShop_OrderAttributeDAO");
+			foreach(SOYShop_OrderAttributeConfig::load() as $config){
+				//管理画面側なら必ずフォームを表示する or 公開側の場合はisAdminOnlyが0であれば表示する
+				if(
+					(defined("SOYSHOP_ADMIN_PAGE") && SOYSHOP_ADMIN_PAGE) ||
+					($config->getIsAdminOnly() != SOYShop_OrderAttribute::DISPLAY_ADMIN_ONLY)
+				) {
+					$this->list[] = $config;
+				}
+			}
+		}
+	}
+
+	function setParameter($params){
+		self::prepare();
+		if(count($this->list)){
+			$queries = array();
+			$binds = array();
+
+			foreach($this->list as $field){
+				$where = array();
+
+				switch($field->getType()){
+					case SOYShop_OrderAttribute::CUSTOMFIELD_TYPE_SELECT:
+						if(isset($params[$field->getFieldId()]) && strlen($params[$field->getFieldId()])){
+							$where[] = "order_value1 = :" . $field->getFieldId();
+							$binds[":" . $field->getFieldId()] = $params[$field->getFieldId()];
+						}
+						break;
+				}
+
+				if(count($where)){
+					$queries[] = "id IN (SELECT order_id FROM soyshop_order_attribute WHERE order_field_id = '" . $field->getFieldId() . "' AND " . implode(" AND ", $where) . ")";
+				}
+			}
+
+			if(count($queries)) return array("queries" => $queries, "binds" => $binds);
+		}
+	}
+
+	function searchItems($params){
+		self::prepare();
+		if(count($this->list)){
+			$array = array();
+			foreach($this->list as $field){
+				//セレクトボックス以外は今の所なし
+				if($field->getType() != SOYShop_OrderAttribute::CUSTOMFIELD_TYPE_SELECT) continue;
+
+				$html = array();
+				switch($field->getType()){
+					case SOYShop_OrderAttribute::CUSTOMFIELD_TYPE_SELECT:
+						$html[] = "<select name=\"search[customs][common_order_customfield][" . $field->getFieldId() . "]\">";
+						$html[] = "	<option></option>";
+						$cnf = $field->getConfig();
+						if(isset($cnf["option"])){
+							$opts = explode("\n", $cnf["option"]);
+							if(count($opts)){
+								foreach($opts as $opt){
+									$opt = trim($opt);
+									if(!strlen($opt)) continue;
+									if(isset($params[$field->getFieldId()]) && $params[$field->getFieldId()] == $opt){
+										$html[] = "	<option selected=\"selected\">" . $opt . "</option>";
+									}else{
+										$html[] = "	<option>" . $opt . "</option>";
+									}
+								}
+							}
+						}
+						$html[] = "</select>";
+						break;
+				}
+
+				if(count($html)) $array[$field->getFieldId()] = array("label" => $field->getLabel(), "form" => implode("\n", $html));
+			}
+
+			return $array;
+		}
+	}
+}
+SOYShopPlugin::extension("soyshop.order.search", "common_order_customfield", "OrderCustomfieldOrderSearch");
