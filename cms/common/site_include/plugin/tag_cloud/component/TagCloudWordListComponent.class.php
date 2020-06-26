f08eb60a83b1367a2270f2f99541e2d66ac72823
inunosinsi <tsuyoshi@saitodev.co>
2020-06-26 10:39:18 +0900

    タグクラウドプラグインのタグ一覧でword_idとhashのみを出力するcms:idを追加

--- cms/common/site_include/plugin/tag_cloud/component/TagCloudWordListComponent.class.php
+++ cms/common/site_include/plugin/tag_cloud/component/TagCloudWordListComponent.class.php
@@ -6,16 +6,30 @@ class TagCloudWordListComponent extends HTMLList {
 	private $ranks; //タグの使用頻度が何位でクラスのrank01を次の数字にするか？
 
 	protected function populateItem($entity, $key, $int){
+		$wordId = (isset($entity["word_id"]) && is_numeric($entity["word_id"])) ? (int)$entity["word_id"] : null;
+
+		$this->addLabel("tag_word_id", array(
+			"soy2prefix" => "cms",
+			"text" => $wordId
+		));
+
 		$rank = (isset($entity["word_id"]) && isset($this->ranks[$entity["word_id"]])) ? $this->ranks[$entity["word_id"]] : 1;
 		$this->addLink("tag_link", array(
 			"soy2prefix" => "cms",
-			"link" => (isset($entity["word_id"]) && is_numeric($entity["word_id"])) ? $this->url . "?tagcloud=" . $entity["word_id"] : "",
+			"link" => (is_numeric($wordId)) ? $this->url . "?tagcloud=" . $wordId : "",
 			"attr:class" => self::_buildClass($rank)
+		));
+
+		$hash = (isset($entity["hash"]) && is_string($entity["hash"])) ? $entity["hash"] : null;
+
+		$this->addLabel("tag_word_hash", array(
+			"soy2prefix" => "cms",
+			"text" => $wordId
 		));
 
 		$this->addLink("tag_hash_link", array(
 			"soy2prefix" => "cms",
-			"link" => (isset($entity["hash"]) && is_string($entity["hash"])) ? $this->url . "?tagcloud=" . $entity["hash"] : "",
+			"link" => (strlen($hash)) ? $this->url . "?tagcloud=" . $hash : "",
 			"attr:class" => self::_buildClass($rank)
 		));
 
