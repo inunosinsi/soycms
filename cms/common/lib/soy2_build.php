b7b769a35e0352b77bb19c511f0837f043690998
inunosinsi <tsuyoshi@saitodev.co>
2020-10-11 06:42:44 +0900

    [Fix]Exclude pathinfo parameters in referrer check.

--- cms/common/lib/soy2_build.php
+++ cms/common/lib/soy2_build.php
@@ -9232,6 +9232,27 @@ function soy2_check_referer(){
 	if($referer['host'] . $port !== $_SERVER['HTTP_HOST']) return false;
 
 	$_path = $referer["path"];
-	if(isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) && strpos($_SERVER['QUERY_STRING'], "pathinfo=") === false) $_path .= "?" . $_SERVER['QUERY_STRING'];
+
+	//pathinfoがある時は削除する
+	$queryString = $_SERVER['QUERY_STRING'];
+	if(is_numeric(strpos($queryString, "pathinfo"))){
+		$array = explode("&", $queryString);
+		$params = array();
+		foreach($array as $value){
+			$v = explode("=", $value);
+			if($v[0] == "pathinfo") continue;
+			$params[$v[0]] = $v[1];
+		}
+
+		$queryString = "";
+		if(count($params)){
+			foreach($params as $key => $v){
+				if(strlen($queryString)) $queryString .= "&";
+				$queryString .= $key . "=" . $v;
+			}
+		}
+	}
+
+	if(isset($queryString) && strlen($queryString)) $_path .= "?" . $queryString;
 	return ($_path == $_SERVER['REQUEST_URI']);
 }
