d441e31caa95fdd95bae6180ac6f3f20cd278005
inunosinsi <tsuyoshi@saitodev.co>
2020-10-11 06:41:31 +0900

    [Add]Add ID for test code.

--- cms/soyshop/webapp/pages/_common/Site/TemplateListComponent.class.php
+++ cms/soyshop/webapp/pages/_common/Site/TemplateListComponent.class.php
@@ -16,15 +16,23 @@ class TemplateListComponent extends HTMLList{
 		));
 
 		$this->addLink("edit_link", array(
-			"link" => $this->getDetailLink() . $entity["path"]
+			"link" => $this->getDetailLink() . $entity["path"],
+			"attr:id" => (is_array($entity)) ? self::_getTemplateId($entity) : null
 		));
-		
+
 		$path = str_replace(".html", "", $entity["path"]);
 		$templateType = substr($path, 0, strpos($path, "/"));
 		$templateId = substr($path, strpos($path, "/") + 1);
 		$this->addLink("remove_link", array(
 			"link" => SOY2PageController::createLink("Site.Template.Remove.?type=" . $templateType . "&id=" . $templateId)
 		));
+	}
+
+	private function _getTemplateId($array){
+		if(!is_array($array) || !count($array) || !isset($array["path"]) || !strlen($array["path"])) return null;
+
+		$id = str_replace("/", "_", $array["path"]);
+		return str_replace(".html", "", $id);
 	}
 
 	var $detailLink;
