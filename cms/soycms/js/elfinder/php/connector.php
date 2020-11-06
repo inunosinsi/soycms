8d54f69ec01a5f35187385416911ddc9aa916f2b
inunosinsi <tsuyoshi@saitodev.co>
2020-11-07 07:38:26 +0900

    [Add]allow css file edit.

--- cms/soycms/js/elfinder/php/connector.php
+++ cms/soycms/js/elfinder/php/connector.php
@@ -216,7 +216,7 @@ $opts = array(
 			'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/../files/.trash/.tmb/',
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
-			'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain'), // Same as above
+			'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain', 'text/css', "application/pdf"), // Same as above
 			'uploadOrder'   => array('deny', 'allow'),      // Same as above
 			'accessControl' => 'access',                    // Same as above
 		),
