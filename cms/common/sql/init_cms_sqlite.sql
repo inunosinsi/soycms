a65cc81222f926c94fc6c4163159f7bf668b232b
inunosinsi <tsuyoshi@saitodev.co>
2021-02-16 18:15:01 +0900

    [Fix]Fix AutoLogin schema.

--- cms/common/sql/init_cms_sqlite.sql
+++ cms/common/sql/init_cms_sqlite.sql
@@ -70,7 +70,7 @@ CREATE TABLE AutoLogin (
 	user_id INTEGER NOT NULL,
 	token CHAR(32) NOT NULL,
 	time_limit INTEGER,
-	UNIQUE(user_id, session_token)
+	UNIQUE(user_id, token)
 );
 
 CREATE TABLE Memo(
--- cms/common/sql/update/admin/sqlite/update-10.sql
+++ cms/common/sql/update/admin/sqlite/update-10.sql
@@ -2,5 +2,5 @@ CREATE TABLE AutoLogin (
 	user_id INTEGER NOT NULL,
 	token CHAR(32) NOT NULL,
 	time_limit INTEGER,
-	UNIQUE(user_id, session_token)
+	UNIQUE(user_id, token)
 );
