6151bcdab742a00a0157a3489c03c58bfcd407aa
inunosinsi <tsuyoshi@saitodev.co>
2021-03-16 13:39:21 +0900

    refactaring

--- cms/app/webapp/inquiry/src/columns/SOYShopColumn.class.php
+++ cms/app/webapp/inquiry/src/columns/SOYShopColumn.class.php
@@ -6,46 +6,37 @@ class SOYShopColumn extends SOYInquiry_ColumnBase{
 	 * ユーザに表示するようのフォーム
 	 */
 	function getForm($attr = array()){
-
-		$itemName = "";
-
-		if(SOYInquiryUtil::checkSOYShopInstall() && isset($_GET["item_id"]) && is_numeric($_GET["item_id"])){
-			$itemId = (int)$_GET["item_id"];
-
-			if(SOYINQUERY_SOYSHOP_CONNECT_SITE_ID){
-				$shopId = SOYInquiryUtil::getSOYShopSiteId();
-				$old = SOYInquiryUtil::switchSOYShopConfig($shopId);
-
-				$itemName = $this->getItemName($itemId);
-
-				SOYInquiryUtil::resetConfig($old);
-			}
+		$itemName = null;
+		if(SOYInquiryUtil::checkSOYShopInstall() && SOYINQUERY_SOYSHOP_CONNECT_SITE_ID && isset($_GET["item_id"]) && is_numeric($_GET["item_id"])){
+			$itemName = self::_getItemName($itemId);
 		}
 
+		if(strlen($itemName)) $itemName = htmlspecialchars($itemName, ENT_QUOTES, "UTF-8");
 		$html = array();
-		$html[] = htmlspecialchars($itemName,ENT_QUOTES,"UTF-8");
-		$html[] = "<input type=\"hidden\" name=\"data[" . $this->getColumnId() . "]\" value=\"" . htmlspecialchars($itemName,ENT_QUOTES,"UTF-8") . "\" />";
+		$html[] = $itemName;
+		$html[] = "<input type=\"hidden\" name=\"data[" . $this->getColumnId() . "]\" value=\"" . $itemName . "\" />";
 
 		return implode("\n",$html);
 
 	}
 
-	function getItemName($itemId){
-		$itemDao = SOY2DAOFactory::create("shop.SOYShop_ItemDAO");
+	private function _getItemName(){
+		$old = SOYInquiryUtil::switchSOYShopConfig(SOYInquiryUtil::getSOYShopSiteId());
+		$name = null;
 		try{
-			$item = $itemDao->getById($itemId);
+			$item = SOY2DAOFactory::create("shop.SOYShop_ItemDAO")->getById((int)$_GET["item_id"]);
+			if($item->isPublished()) $name = trim($item->getOpenItemName());
 		}catch(Exception $e){
-			return "";
+			//
 		}
-
-		return $item->getName();
+		SOYInquiryUtil::resetConfig($old);
+		return $name;
 	}
 
 	/**
 	 * 設定画面で表示する用のフォーム
 	 */
-	function getConfigForm(){
-	}
+	function getConfigForm(){}
 
 	/**
 	 * 保存された設定値を渡す
@@ -55,10 +46,8 @@ class SOYShopColumn extends SOYInquiry_ColumnBase{
 	}
 
 	function getConfigure(){
-		$config = parent::getConfigure();
-		return $config;
+		return parent::getConfigure();
 	}
 
-	function validate(){
-	}
+	function validate(){}
 }
