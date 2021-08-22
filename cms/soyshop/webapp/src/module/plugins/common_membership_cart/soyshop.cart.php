bd1dd9e892680052d01b598fc737b1b8f83333c3
inunosinsi <tsuyoshi@saitodev.co>
2021-08-22 11:38:01 +0900

    [Refactoring]

--- cms/soyshop/webapp/src/module/plugins/common_membership_cart/soyshop.cart.php
+++ cms/soyshop/webapp/src/module/plugins/common_membership_cart/soyshop.cart.php
@@ -2,8 +2,7 @@
 class CommonMembershipCart extends SOYShopCartBase{
 
 	function displayPage03(CartLogic $cart){
-		
+
 	}
 }
 SOYShopPlugin::extension("soyshop.cart","common_membership_cart","CommonMembershipCart");
-?>
