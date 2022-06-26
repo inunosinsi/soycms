27b93cf5eeec8c2617b235ccefda622c61e2a040
inunosinsi <tsuyoshi@saitodev.co>
2022-06-27 05:59:55 +0900

    Supported PHP8.1.

---
+++ cms/soycms/webapp/pages/_component/Template/HistoryListComponent.class.php
@@ -0,0 +1,36 @@
+<?php
+
+class HistoryListComponent extends HTMLList{
+
+	private $mode;
+	private $pageId;
+
+	public function setMode($mode){
+		$this->mode = $mode;
+	}
+	public function setPageId($id){
+		$this->pageId = $id;
+	}
+
+	public function populateItem($entity){
+		$contents = unserialize($entity->getContents());
+		$contents = (string)$contents[$this->mode];
+		
+		$this->addLink("date", array(
+			"link" => SOY2PageController::createLink("Blog.TemplateHistoryDetail.{$this->pageId}.{$entity->getId()}.{$this->mode}"),
+			"text"=> date("Y-m-d H:i:s", (int)$entity->getUpdateDate())
+		));
+
+		$this->addLabel("contents", array(
+			"text"  => mb_strimwidth(strip_tags($contents),0,128,'...'),
+			"title" => mb_strimwidth(strip_tags($contents),0,1000,'...'),
+		));
+
+		$this->addForm("restoreForm");
+
+		$this->addInput("historyId", array(
+			"name"=>"historyId",
+			"value"=>$entity->getId()
+		));
+	}
+}
