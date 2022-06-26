817c8a2e578d61e557151e60f9130d2d4f6cd78d
inunosinsi <tsuyoshi@saitodev.co>
2022-06-27 05:58:26 +0900

    Supported PHP8.1.

--- cms/soycms/webapp/pages/Blog/TemplateHistoryPage.class.php
+++ cms/soycms/webapp/pages/Blog/TemplateHistoryPage.class.php
@@ -33,46 +33,10 @@ class TemplateHistoryPage extends CMSWebPageBase {
 
 		$list = $result->getAttribute("historyList");
 
-		$this->createAdd("templateList","HistoryList",array(
+		$this->createAdd("templateList", "_component.Template.HistoryListComponent", array(
 			"list"   => $list,
 			"mode"   => $this->mode,
 			"pageId" => $this->pageId
-		));
-
-	}
-}
-
-class HistoryList extends HTMLList{
-
-	private $mode;
-	private $pageId;
-
-	public function setMode($mode){
-		$this->mode = $mode;
-	}
-	public function setPageId($id){
-		$this->pageId = $id;
-	}
-
-	public function populateItem($entity){
-		$contents = unserialize($entity->getContents());
-		$contents = $contents[$this->mode];
-
-		$this->createAdd("date","HTMLLink",array(
-			"link" => SOY2PageController::createLink("Blog.TemplateHistoryDetail.{$this->pageId}.{$entity->getId()}.{$this->mode}"),
-			"text"=> date("Y-m-d H:i:s", (int)$entity->getUpdateDate())
-		));
-
-		$this->createAdd("contents","HTMLLabel",array(
-			"text"  => mb_strimwidth(strip_tags($contents),0,128,'...'),
-			"title" => mb_strimwidth(strip_tags($contents),0,1000,'...'),
-		));
-
-		$this->createAdd("restoreForm","HTMLForm");
-
-		$this->createAdd("historyId","HTMLInput",array(
-			"name"=>"historyId",
-			"value"=>$entity->getId()
 		));
 	}
 }
