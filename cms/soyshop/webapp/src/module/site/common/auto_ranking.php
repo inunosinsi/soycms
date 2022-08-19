5e50c37e86c85070053f43bc74de76c0f26e6d91
inunosinsi <tsuyoshi@saitodev.co>
2022-08-19 10:28:15 +0900

    [Refactoring]

--- cms/soyshop/webapp/src/module/site/common/auto_ranking.php
+++ cms/soyshop/webapp/src/module/site/common/auto_ranking.php
@@ -4,16 +4,21 @@ function soyshop_auto_ranking($html, $page){
 	$obj = $page->create("soyshop_auto_ranking", "HTMLTemplatePage", array(
 		"arguments" => array("soyshop_auto_ranking", $html)
 	));
+
+	// cms:customfield="off" の場合はカスタムフィールドの拡張ポイントを実行しない
+	$isCallCustomfield = true;
+	if(preg_match('/cms:customfield=\"(.*)\"/', $html, $tmp)){
+		if(isset($tmp[1]) && $tmp[1] == "off") $isCallCustomfield = false;
+	}
 	
-	$displayLogic = SOY2Logic::createInstance("module.plugins.common_auto_ranking.logic.DisplayRankingLogic");
-	$items = $displayLogic->getItems();
+	$items = SOY2Logic::createInstance("module.plugins.common_auto_ranking.logic.DisplayRankingLogic")->getItems();
 	
 	SOY2::import("base.site.classes.SOYShop_ItemListComponent");
 	$obj->createAdd("ranking_item_list", "SOYShop_ItemListComponent", array(
 		"soy2prefix" => "block",
-		"list" => $items
+		"list" => $items,
+		"isCallCustomfield" => $isCallCustomfield
 	));
 		
 	$obj->display();
 }
-?>
