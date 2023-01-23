4cc9372ed114d33a179411f98bf5eff86f9af52e
inunosinsi <tsuyoshi@saitodev.co>
2023-01-23 15:00:17 +0900

    [Refactoring]

--- cms/common/logic/site/Module/ModuleCreateLogic.class.php
+++ cms/common/logic/site/Module/ModuleCreateLogic.class.php
@@ -2,8 +2,12 @@
 
 class ModuleCreateLogic extends SOY2LogicBase{
 
-	//モジュール名に?{}|&~!()^"が含まれている場合はエラーを返す
-	function validate($moduleName){
+	/**
+	 * モジュール名に?{}|&~!()^"が含まれている場合はエラーを返す
+	 * @param string
+	 * @return bool
+	 */
+	function validate(string $moduleName){
 		if(preg_match('/\?|{|}|&|~|!|\(|\)|\^|"/', $moduleName, $tmp)){
 			return false;
 		}
