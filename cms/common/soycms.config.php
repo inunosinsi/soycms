ef4b2acc9c7051b356c9eb6b090a8cf7015de2b5
inunosinsi <tsuyoshi@saitodev.co>
2023-03-11 05:11:38 +0900

    [Upgrade]

--- cms/common/soycms.config.php
+++ cms/common/soycms.config.php
@@ -1,8 +1,8 @@
 <?php
 define("SOYCMS_DB_TYPE","mysql");
-define("SOYCMS_VERSION","3.11.14");
-define("SOYCMS_BUILD","2023-03-10T10:36:28+09:00");
-define("SOYCMS_RAW",20230310103628);	//Apache Ant(JDK17)対策 ここでしか使わない定数
+define("SOYCMS_VERSION","3.11.15");
+define("SOYCMS_BUILD","2023-03-11T05:10:23+09:00");
+define("SOYCMS_RAW",20230311051023);	//Apache Ant(JDK17)対策 ここでしか使わない定数
 define("SOYCMS_BUILD_TIME", mktime(substr(SOYCMS_RAW, 8, 2), substr(SOYCMS_RAW, 10, 2), substr(SOYCMS_RAW, 12), substr(SOYCMS_RAW, 4, 2), substr(SOYCMS_RAW, 6, 2), substr(SOYCMS_RAW, 0, 4)));
 define("SOYCMS_REVISION","45154");
 define("SOYCMS_AUTOLOGIN_EXPIRE", 30);
