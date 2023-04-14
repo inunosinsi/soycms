0236b10a713d6890c123cce4e71b109f9b8d2918
inunosinsi <tsuyoshi@saitodev.co>
2023-04-14 11:49:05 +0900

    [Add]Added soy2_strpos function.

--- cms/soyshop/webapp/lib/soy2_build.php
+++ cms/soyshop/webapp/lib/soy2_build.php
@@ -9297,3 +9297,30 @@ function soy2_number_format($int){
 	if(!is_numeric($int)) return 0;
 	return number_format($int);
 }
+
+/* function/function.soy2_strpos.php */
+/**
+ * 文字列中に、ある部分文字列が最初に現れる場所を探す ヒットしない場合は-1を返す
+ */
+function soy2_strpos(string $haystack, string $needle, int $offset = 0){
+	$res = strpos($haystack, $needle, $offset);
+	return (is_numeric($res)) ? $res : -1;
+}
+
+/**
+ * 文字列中に、ある部分文字列が最後に現れる場所を探す ヒットしない場合は-1を返す
+ */
+function soy2_strrpos(string $haystack, string $needle, int $offset = 0){
+	$res = strrpos($haystack, $needle, $offset);
+	return (is_numeric($res)) ? $res : -1;
+}
+
+function soy2_stripos(string $haystack, string $needle, int $offset = 0){
+	$res = stripos($haystack, $needle, $offset);
+	return (is_numeric($res)) ? $res : -1;
+}
+
+function soy2_strripos(string $haystack, string $needle, int $offset = 0){
+	$res = strripos($haystack, $needle, $offset);
+	return (is_numeric($res)) ? $res : -1;
+}
