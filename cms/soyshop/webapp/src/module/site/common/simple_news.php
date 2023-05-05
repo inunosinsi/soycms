b009647bf423ccea4f80ab7963c046afc854bf8a
inunosinsi <tsuyoshi@saitodev.co>
2023-05-06 04:50:16 +0900

    [Refactoring]

--- cms/soyshop/webapp/src/module/site/common/simple_news.php
+++ cms/soyshop/webapp/src/module/site/common/simple_news.php
@@ -1,24 +1,35 @@
 <?php
+/**
+ * @param string
+ * @return bool
+ */
+function soyshop_check_used_block_type(string $html){
+	$lines = explode("\n", $html);
 
-function soyshop_simple_news($html, $htmlObj){
-
-
+	foreach($lines as $line){
+		preg_match('/block:id=\"news_list\"/', $line, $tmp);
+		if(isset($tmp[0])) return true;
+	}
+	return false;
+}
+function soyshop_simple_news(string $html, HTMLPage $htmlObj){
 	$obj = $htmlObj->create("soyshop_simple_news", "HTMLTemplatePage", array(
 		"arguments" => array("soyshop_simple_news", $html)
 	));
 
 	$news = SOYShop_DataSets::get("plugin.simple_news", array());
 	
-	$obj->createAdd("news_list", "SimpleNewsListComponent", array(
-		"list" => $news,
-		"soy2prefix" => SOYSHOP_SITE_PREFIX,//互換性のため残しておく
-	));
-
-	$obj->createAdd("news_list", "SimpleNewsListComponent", array(
-		"list" => $news,
-		"soy2prefix" => "block",
-	));
-
+	if(!soyshop_check_used_block_type($html)){
+		$obj->createAdd("news_list", "SimpleNewsListComponent", array(
+			"list" => $news,
+			"soy2prefix" => SOYSHOP_SITE_PREFIX,//互換性のため残しておく
+		));
+	}else{
+		$obj->createAdd("news_list", "SimpleNewsListComponent", array(
+			"list" => $news,
+			"soy2prefix" => "block",
+		));
+	}	
 
 	//商品があるときだけ表示
 	if(is_array($news) && count($news) > 0){
@@ -37,7 +48,7 @@ if(!class_exists("SimpleNewsListComponent")){
 			$url = (isset($entity["url"]) && is_string($entity["url"])) ? $entity["url"] : "";
 			if(strlen($url) && preg_match('/\/\/$/', $url)) $url = "";	// $urlが//で終わっている場合は空文字にする
 			
-			$createDate = (isset($entity["create_date"]) && is_string($entity["create_date"]) && preg_match('/[\d]{4}-[\d]{1,2}-[\d]{1,2}/', $entity["create_date"])) ? $entity["create_date"] : "";
+			$createDate = (isset($entity["create_date"]) && is_string($entity["create_date"])) ? $entity["create_date"] : "";
 
 			$this->addLabel("create_date", array(
 				"soy2prefix" => SOYSHOP_SITE_PREFIX, 
